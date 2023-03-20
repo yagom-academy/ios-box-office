2e27ce7b2b953db87ef90f69294e7a9ce09445ac
HarryHyeon <hharry0728@gmail.com>
2023-03-20 15:14:50 +0900

    feat: BoxOffice 모델에 CodingKeys 추가

--- BoxOffice/BoxOffice.swift
+++ BoxOffice/BoxOffice.swift
@@ -5,21 +5,47 @@
 //  Created by kaki, harry on 2023/03/20.
 //
 
-struct BoxOfficeResult: Codable {
+struct BoxOffice: Codable {
     let boxofficeType, showRange: String
     let dailyBoxOfficeList: [DailyBoxOfficeList]
 }
 
 struct DailyBoxOfficeList: Codable {
-    let rnum, rank, rankInten: String
+    let number, rank, rankIncrement: String
     let rankOldAndNew: RankOldAndNew
-    let movieCd, movieNm, openDt, salesAmt: String
-    let salesShare, salesInten, salesChange, salesAcc: String
-    let audiCnt, audiInten, audiChange, audiAcc: String
-    let scrnCnt, showCnt: String
+    let moviewCode, movieKoreanName, openDate, salesAmount: String
+    let salesShare, salesIncrement, salesChange, salesAccumulation: String
+    let audienceCount, audienceIncrement, audienceChange, audienceAccumulation: String
+    let screenCount, showCount: String
+    
+    enum CodingKeys: String, CodingKey {
+        case rank, rankOldAndNew, salesShare, salesChange
+        case number = "rnum"
+        case rankIncrement = "rankInten"
+        case moviewCode = "movieCd"
+        case movieKoreanName = "movieNm"
+        case openDate = "opendDt"
+        case salesAmount = "salesAmt"
+        case salesIncrement = "salesInten"
+        case salesAccumulation = "salesAcc"
+        case audienceCount = "audiCnt"
+        case audienceIncrement = "audiInten"
+        case audienceChange = "audiChange"
+        case audienceAccumulation = "audiAcc"
+        case screenCount = "scrnCnt"
+        case showCount = "showCnt"        
+    }
 }
 
 enum RankOldAndNew: String, Codable {
     case new = "NEW"
     case old = "OLD"
 }
+
+extension BoxOffice: Equatable {
+    public static func == (lhs: BoxOffice, rhs: BoxOffice) -> Bool {
+            return lhs.boxofficeType == rhs.boxofficeType && lhs.showRange == rhs.showRange && lhs.dailyBoxOfficeList == rhs.dailyBoxOfficeList
+        }
+}
+
+
---
+++ BoxOfficeTests/BoxOfficeTests.swift
@@ -0,0 +1,17 @@
+//
+//  BoxOfficeTests.swift
+//  BoxOfficeTests
+//
+//  Created by Harry on 2023/03/20.
+//
+
+import XCTest
+
+final class BoxOfficeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
