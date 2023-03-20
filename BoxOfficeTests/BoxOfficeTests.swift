cbdd805d45ea63ad0865a6f3a6f2a8181e0da602
sehong <tjsgh0801@naver.com>
2023-03-20 19:02:18 +0900

    Test: Test 케이스 수정 및 추가

--- BoxOfficeTests/BoxOfficeTests.swift
+++ BoxOfficeTests/BoxOfficeTests.swift
@@ -13,21 +13,33 @@ final class BoxOfficeTests: XCTestCase {
     private var decoder: JSONDecoder?
     
     override func setUpWithError() throws {
-      try super.setUpWithError()
-      decoder = JSONDecoder()
+        try super.setUpWithError()
+        decoder = JSONDecoder()
     }
     
     override func tearDownWithError() throws {
-      try super.tearDownWithError()
-      decoder = nil
+        try super.tearDownWithError()
+        decoder = nil
     }
     
     func testDecode_IfBoxOfficeDataProvide_NoThrowError() {
         // given when then
-        if let path = Bundle.main.path(forResource: "box_office_sample", ofType: "json"),
-           let data = try? Data(contentsOf: URL(fileURLWithPath: path)) {
-            XCTAssertNoThrow(try decoder?.decode(BoxOfficItem.self, from: data))
+        guard let asset = NSDataAsset(name: "box_office_sample", bundle: .main) else {
+            XCTFail("Decode Failure")
+            return
         }
+        XCTAssertNoThrow(try decoder?.decode(BoxOfficItem.self, from: asset.data))
     }
     
+    func testDecode_ifOfficeDataProvide_ReturnOfficeType() {
+       // given when then
+       guard let asset = NSDataAsset(name: "box_office_sample", bundle: .main),
+             let boxOffice = try? decoder?.decode(BoxOfficItem.self, from: asset.data)
+       else {
+         XCTFail("Decode Failure")
+         return
+       }
+        XCTAssertEqual(boxOffice.boxOfficeResult.boxofficeType, "일별 박스오피스")
+     }
+
 }
