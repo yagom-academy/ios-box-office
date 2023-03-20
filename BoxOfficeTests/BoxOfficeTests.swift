77ba6e5fae3a47615b6d0871ad0337ea297415a6
Jun <59365211+juun97@users.noreply.github.com>
2023-03-20 21:24:38 +0900

    feat : BoxOffice UnitTest 진행

--- BoxOfficeTests/BoxOfficeTests.swift
+++ BoxOfficeTests/BoxOfficeTests.swift
@@ -24,6 +24,7 @@ final class BoxOfficeTests: XCTestCase {
         
         //when
         let result = sut.decodeBoxOffice()
+        
         //then
         XCTAssertNotNil(result)
     }
@@ -33,6 +34,7 @@ final class BoxOfficeTests: XCTestCase {
         
         //when
         let result = sut.decodeBoxOffice()?.boxOfficeResult
+        
         //then
         XCTAssertNotNil(result)
     }
@@ -42,6 +44,7 @@ final class BoxOfficeTests: XCTestCase {
         
         //when
         let result = sut.decodeBoxOffice()?.boxOfficeResult.dailyBoxOfficeList
+        
         //then
         XCTAssertNotNil(result)
     }
@@ -52,6 +55,7 @@ final class BoxOfficeTests: XCTestCase {
         //when
         let result = sut.decodeBoxOffice()?.boxOfficeResult.dailyBoxOfficeList.count
         let expectedResult = 10
+        
         //then
         XCTAssertEqual(result, expectedResult)
     }
@@ -59,8 +63,10 @@ final class BoxOfficeTests: XCTestCase {
     func test_디코딩시_첫번째인덱스의number의value가Int형태를가진다() {
         //given
         guard let number = sut.decodeBoxOffice()?.boxOfficeResult.dailyBoxOfficeList[0].number else { return }
+        
         //when
         let result = Int(number)
+        
         //then
         XCTAssertNotNil(result)
     }
@@ -68,9 +74,51 @@ final class BoxOfficeTests: XCTestCase {
     func test_디코딩시_첫번째인덱스의salesShare의value가Double형태를가진다() {
         //given
         guard let number = sut.decodeBoxOffice()?.boxOfficeResult.dailyBoxOfficeList[0].salesShare else { return }
+        
         //when
         let result = Double(number)
+        
         //then
         XCTAssertNotNil(result)
+    }
+    
+    func test_dailyBoxOfficeList의_rank가1위인영화는_경관의피이다() {
+        //given
+        let dailyBoxOfficeList = sut.decodeBoxOffice()?.boxOfficeResult.dailyBoxOfficeList
+        
+        //when
+        let expectedResult = "경관의 피"
+        let resultBoxOfficeList = dailyBoxOfficeList?.filter{ boxOffice in
+            boxOffice.rank == "1"
+        }
+        
+        let result = resultBoxOfficeList?.first
+        
+        //then
+        XCTAssertEqual(result?.movieName, expectedResult)
+    }
+    
+    func test_dailyBoxOfficeList의_rank가높은영화는_rank가낮은영화보다_SalesAccumulation이높다() {
+        //given
+        guard let dailyBoxOfficeList = sut.decodeBoxOffice()?.boxOfficeResult.dailyBoxOfficeList else { return }
+ 
+        //when
+        var result: Bool?
+        
+        for index in 0..<dailyBoxOfficeList.count {
+            guard let previous =  Int(dailyBoxOfficeList[index].salesAccumulation),
+                  let next = Int(dailyBoxOfficeList[index+1].salesAccumulation) else { return }
+            
+            guard previous > next else {
+                result = false
+                return
+            }
+            result = true
+        }
+        
+        let expectedResult = true
+        
+        //then
+        XCTAssertEqual(result, expectedResult)
     }
 }
