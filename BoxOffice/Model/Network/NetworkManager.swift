2d3bdd009bb55559c9e231b4e110d506c6403195
HarryHyeon <hharry0728@gmail.com>
2023-03-21 15:38:18 +0900

    feat: NetworkError 정의, 폴더 구조 정리

---
+++ BoxOffice/Model/Network/NetworkManager.swift
@@ -0,0 +1,23 @@
+//
+//  NetworkManager.swift
+//  BoxOffice
+//
+//  Created by Harry, kaki on 2023/03/21.
+//
+
+import Foundation
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    private let session = URLSession.shared
+    
+    private init() {}
+    
+    func getDailyChart() {
+        
+    }
+    
+    func getMovieInfo() {
+        
+    }
+}
