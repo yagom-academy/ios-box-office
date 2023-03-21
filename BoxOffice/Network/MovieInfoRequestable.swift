235f4f102adaa80b7d2dde2c67bcaf1b70d8861f
kkuldak1@naver.com <kkuldak1@naver.com>
2023-03-21 12:26:50 +0900

    feat: DailyBoxOfficeRequestable, MovieInfoRequestable 프로토콜 구현, MovieType, NationalCode enum 구현

---
+++ BoxOffice/Network/MovieInfoRequestable.swift
@@ -0,0 +1,11 @@
+//
+//  MovieInfoRequestable.swift
+//  BoxOffice
+//
+//  Created by 리지, kokkilE on 2023/03/21.
+//
+
+protocol MovieInfoRequestable {
+    var key: String { get }
+    var movieCode: String { get }
+}
---
+++ BoxOffice/Network/RequestInterfaceElements.swift
@@ -0,0 +1,16 @@
+//
+//  RequestInterfaceElements.swift
+//  BoxOffice
+//
+//  Created by 리지, kokkilE on 2023/03/21.
+//
+
+enum MovieType: String {
+    case diversity = "Y"
+    case commercial = "N"
+}
+
+enum NationalCode: String {
+    case korean = "K"
+    case foreign = "F"
+}
