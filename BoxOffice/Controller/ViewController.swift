cec407197f3af45ae14e53843e9726269cb2cc7b
Rhode-park <jinah.park.16@gmail.com>
2023-03-21 14:28:38 +0900

    refactor: decoder 내부 decodeBoxOffice 메서드 수정 및 url을 통한 data받아오기 확인

--- BoxOffice/Controller/ViewController.swift
+++ BoxOffice/Controller/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     let decoder = Decoder()
+    let server = ServerCommunication()
+    let url = CommunicationForm.dailyBoxOffice + "20230320"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let boxOfficeResult = decoder.decodeBoxOffice()
+        print(url)
+        server.startLoad(urlText: url) { data in
+            let decodedData = self.decoder.decodeBoxOffice(data: data)
+            print(decodedData?.boxOfficeResult.dailyBoxOfficeList)
+        }
     }
 
 
