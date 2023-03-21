cd8782e7c08ffc1155cc137fc4326e5acf5724ec
kimseongj <ksj8277@naver.com>
2023-03-21 14:26:14 +0900

    refactor : 데이터 모델 CodingKeys를 통해 상수명 수정 및 DailyBoxOfficeList 상수명을 DailyBoxOffice로 변경

--- BoxOffice/Model/BoxOffice.swift
+++ BoxOffice/Model/BoxOffice.swift
@@ -14,7 +14,7 @@ struct BoxOffice: Decodable {
 struct BoxOfficeResult: Decodable {
     let boxOfficeType: String
     let showRange: String
-    let dailyBoxOfficeList: [DailyBoxOfficeList]
+    let dailyBoxOfficeList: [DailyBoxOffice]
     
     private enum CodingKeys: String, CodingKey {
         case boxOfficeType = "boxofficeType"
@@ -23,25 +23,46 @@ struct BoxOfficeResult: Decodable {
     }
 }
 
-struct DailyBoxOfficeList: Decodable {
-    let rnum: String
+struct DailyBoxOffice: Decodable {
+    let movieNumber: String
     let rank: String
-    let rankInten: String
+    let rankGap: String
     let rankOldAndNew: String
-    let movieCd: String
-    let movieNm: String
-    let openDt: String
-    let salesAmt: String
-    let salesShare: String
-    let salesInten: String
-    let salesChange: String
-    let salesAcc: String
-    let audiCnt: String
-    let audiInten: String
-    let audiChange: String
-    let audiAcc: String
-    let scrnCnt: String
-    let showCnt: String
+    let movieCode: String
+    let movieName: String
+    let openDate: String
+    let salesAmount: String
+    let salesPercent: String
+    let salesGap: String
+    let salesGapPercent: String
+    let salesAccumulation: String
+    let audienceCount: String
+    let audienceGap: String
+    let audienceGapPercent: String
+    let audienceAccumulation: String
+    let screenCount: String
+    let showCount: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case movieNumber = "rnum"
+        case rank
+        case rankGap = "rankInten"
+        case rankOldAndNew
+        case movieCode = "movieCd"
+        case movieName = "movieNm"
+        case openDate = "openDt"
+        case salesAmount = "salesAmt"
+        case salesPercent = "salesShare" // 해당일자 판매 비율
+        case salesGap = "salesInten" // 전일 대비 매출액 증감분
+        case salesGapPercent = "salesChange" // 전일 대비 매출애 증감 비율
+        case salesAccumulation = "salesAcc"
+        case audienceCount = "audiCnt"
+        case audienceGap = "audiInten"
+        case audienceGapPercent = "audiChange"
+        case audienceAccumulation = "audiAcc"
+        case screenCount = "screenCnt"
+        case showCount = "showCnt"
+    }
 }
 
 
