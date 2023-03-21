edbf76334f48dfcde7ab1f4578e51deabe53f86d
yijiye <jiyee4013@naver.com>
2023-03-21 17:50:00 +0900

    feat: step1 변경사항 적용

---
+++ BoxOffice/Model/Decoder.swift
@@ -0,0 +1,23 @@
+//
+//  Decoder.swift
+//  BoxOffice
+//
+//  Created by 리지, kokkilE on 2023/03/21.
+//
+
+import UIKit
+
+enum Decoder {
+   static func parseJSON<element: Decodable>(_ fileName: String, returnType: element.Type) -> element? {
+       guard let dataAsset = NSDataAsset(name: fileName) else { return nil }
+       
+       do {
+           let jsonDecoder = JSONDecoder()
+           let result = try jsonDecoder.decode(returnType, from: dataAsset.data)
+           return result
+       } catch {
+           print(error.localizedDescription)
+           return nil
+       }
+   }
+}
--- BoxOfficeTests/BoxOfficeTests.swift
+++ BoxOfficeTests/BoxOfficeTests.swift
@@ -12,22 +12,21 @@ final class BoxOfficeTests: XCTestCase {
     var sut: BoxOffice!
     var fileName = "box_office_sample"
     
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        sut = Decoder.parseJSON(fileName, returnType: BoxOffice.self)
+    }
+    
     override func tearDownWithError() throws {
         try super.tearDownWithError()
-
+        
         sut = nil
     }
     
-    func parseJSON(_ fileName: String) {
-        guard let dataAsset = NSDataAsset(name: fileName) else { return }
-        
-        do {
-            let jsonDecoder = JSONDecoder()
-            sut = try jsonDecoder.decode(BoxOffice.self, from: dataAsset.data)
-        } catch {
-            sut = nil
-            print(error.localizedDescription)
-        }
+    func test_parsing이_제대로동작하면_sut는nil이아니다() {
+        // then
+        XCTAssertNotNil(sut)
     }
     
     func test_잘못된파일명으로_parseJSON호출시_sut는nil이다() {
@@ -35,23 +34,14 @@ final class BoxOfficeTests: XCTestCase {
         let wrongFileName = "wrongFileName"
         
         // when
-        parseJSON(wrongFileName)
+        sut = Decoder.parseJSON(wrongFileName, returnType: BoxOffice.self)
         
         // then
         XCTAssertNil(sut)
     }
     
-    func test_올바른파일명으로_parseJSON호출시_sut는nil이아니다() {
-        // when
-        parseJSON(fileName)
-        
-        // then
-        XCTAssertNotNil(sut)
-    }
-    
     func test_parsing한_movieList의개수는_10개이다() {
         // given
-        parseJSON(fileName)
         let expectation = 10
         
         // when
@@ -63,7 +53,6 @@ final class BoxOfficeTests: XCTestCase {
     
     func test_parsing한_movie_rank1인_영화이름은_경관의피이다() {
         // given
-        parseJSON(fileName)
         let expectation = "경관의 피"
         
         // when
@@ -81,7 +70,6 @@ final class BoxOfficeTests: XCTestCase {
     
     func test_parsing한_movieList의_첫번째movie의프로퍼티는_원본데이터와같다() {
         // given
-        parseJSON(fileName)
         let expectation = ["1", "1", "0", "NEW", "20199882", "경관의 피", "2022-01-05", "584559330", "34.2", "584559330", "100", "631402330", "64050", "64050", "100", "69228", "1171", "4416"]
         
         // when
@@ -111,7 +99,6 @@ final class BoxOfficeTests: XCTestCase {
     
     func test_parsing한_movieList의_rnum을순서대로출력시_원본데이터순서와같다() {
         // given
-        parseJSON(fileName)
         let expectation = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
         
         // when
@@ -119,7 +106,7 @@ final class BoxOfficeTests: XCTestCase {
         sut.boxOfficeResult.boxOfficeList.forEach {
             result.append($0.order)
         }
-
+        
         // then
         XCTAssertEqual(result, expectation)
     }
