6d6e3a09d16b2abbf63a3b6f125ee5952a50fb56
sehong <tjsgh0801@naver.com>
2023-03-22 18:38:17 +0900

    feat: 일일오피스 및 영화 상세 데이터 출력을 위한 함수 구현

--- BoxOffice/ViewController.swift
+++ BoxOffice/ViewController.swift
@@ -2,18 +2,60 @@
 //  ViewController.swift
 //  BoxOffice
 //
-//  Created by kjs on 13/01/23.
+//  Created by Seoyeon Hong on 2023/03/22.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let targetDate = "20220301"
+    let movieCode = "20124079"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchBoxOfficeData()
+        fetchMovieDetailData()
     }
 
+    func fetchBoxOfficeData() {
+        URLSessionProvider.shared.performRequest(api: .boxOffice(date: targetDate)) { requestResult in
+            switch requestResult {
+            case .success(let data):
+                do {
+                    let boxOfficeItem: BoxOfficItem = try JSONConverter.shared.decodeData(data, T: BoxOfficItem.self)
+                    let myMovielists = boxOfficeItem.boxOfficeResult.dailyBoxOfficeList
+                    for movie in myMovielists {
+                        print(movie)
+                    }
+                } catch let error as NetworkError {
+                    print(error.description)
+                } catch {
+                    print("Unexpected error: \(error)")
+                }
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 
+    func fetchMovieDetailData() {
+        URLSessionProvider.shared.performRequest(api: .detail(code: movieCode)) { requestResult in
+            switch requestResult {
+            case .success(let data):
+                do {
+                    let movieInfo: MovieInfo = try JSONConverter.shared.decodeData(data, T: MovieInfo.self)
+                    print(movieInfo)
+                } catch let error as NetworkError {
+                    print(error.description)
+                } catch {
+                    print("Unexpected error: \(error)")
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
 }
-
