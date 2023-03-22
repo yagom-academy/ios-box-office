db40edddbc5b93504a6bb8749ae9bab1aa1516c4
sehong <tjsgh0801@naver.com>
2023-03-22 18:25:47 +0900

    feat:  API 요청을 받아 해당 요청받고 dataTask함수를 호출하여 네트워크 요청을 처리하는 함수 구현

--- BoxOffice/Network/URLSessionProvider.swift
+++ BoxOffice/Network/URLSessionProvider.swift
@@ -32,5 +32,29 @@ final class URLSessionProvider {
         }
         task.resume()
     }
- 
+    
+    func performRequest(api: MovieAPI, completionHandler: @escaping (Result<Data, NetworkError>) -> Void) {
+        guard var urlComponents = URLComponents(string: api.baseURL) else {
+            completionHandler(.failure(.invalidURLComponents))
+            return
+        }
+        urlComponents.path = api.path
+        
+        let keyParam = URLQueryItem(name: "key", value: api.key)
+        
+        var queryParams = api.parameter.map { URLQueryItem(name: $0.key , value: $0.value) }
+        queryParams.append(keyParam)
+        urlComponents.queryItems = queryParams
+        
+        guard let url = urlComponents.url else {
+            completionHandler(.failure(.invalidURLRequest))
+            return
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = api.method
+        
+        dataTask(request: urlRequest, completionHandler: completionHandler)
+                
+    }
 }
