9741cbc0d39f994a1aa719f901f58e5efd0f4bc6
sehong <tjsgh0801@naver.com>
2023-03-22 18:06:51 +0900

    feat: 네트워크 요청 수행하는 함수 구현

--- BoxOffice/Network/URLSessionProvider.swift
+++ BoxOffice/Network/URLSessionProvider.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+final class URLSessionProvider {
+    
+    static let shared = URLSessionProvider()
+    
+    private init() {}
+    
+    private func dataTask(request: URLRequest, completionHandler: @escaping (Result<Data, NetworkError>) -> Void) {
+        let task = URLSession.shared.dataTask(with: request) { data, urlResponse, error in
+            
+            guard error == nil else {
+                completionHandler(.failure(.clientError))
+                return
+            }
+            
+            guard let response = urlResponse as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+                completionHandler(.failure(.serverError))
+                return
+            }
+            
+            if let data = data {
+                return completionHandler(.success(data))
+            }
+        }
+        task.resume()
+    }
+ 
+}
