5eab549f9da5d71c933eeb9aeaabc2a9dc3da64d
Andrew-0411 <whk1140@naver.com>
2023-03-22 16:59:47 +0900

    refactor: BoxofficeError 설명 추가 구현

--- BoxOffice/Model/BoxofficeError.swift
+++ BoxOffice/Model/BoxofficeError.swift
@@ -7,9 +7,22 @@
 
 import Foundation
 
-enum BoxofficeError: Error {
+enum BoxofficeError: LocalizedError {
     case urlError
     case sessionError
     case responseError
     case decodingError
+    
+    var errorDescription: String? {
+        switch self {
+        case .urlError:
+            return "url형식이 잘못되었습니다"
+        case .sessionError:
+            return "네트워크 연결이 불완전합니다"
+        case .responseError:
+            return "데이터를 받아오지 못하였습니다"
+        case .decodingError:
+            return "데이터를 변환하지 못하였습니다"
+        }
+    }
 }
