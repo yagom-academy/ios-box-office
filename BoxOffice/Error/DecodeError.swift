8e1e9fcd7167765e4b6a0105ab579f2bd0149687
Jun <59365211+juun97@users.noreply.github.com>
2023-03-22 14:14:21 +0900

    refactor: STEP 1 변경사항 적용

---
+++ BoxOffice/Error/DecodeError.swift
@@ -0,0 +1,22 @@
+//
+//  DecodeError.swift
+//  BoxOffice
+//
+//  Created by Rhode, Rilla on 2023/03/20.
+//
+
+import Foundation
+
+enum DecodeError: LocalizedError {
+    case invalidFileError
+    case decodingFailureError
+
+    var errorDescription: String? {
+        switch self {
+        case .invalidFileError:
+            return "NSDataAsset으로 변환하기에 적절한 file이 존재하지 않습니다."
+        case .decodingFailureError:
+            return "해당 file을 디코딩할 수 없습니다."
+        }
+    }
+}
---
+++ BoxOffice/Extra/DecodeManager.swift
@@ -0,0 +1,36 @@
+//
+//  Decoder.swift
+//  BoxOffice
+//
+//  Created by Rhode, Rilla on 2023/03/20.
+//
+
+import UIKit
+
+final class DecodeManager {
+    
+    private let decoder = JSONDecoder()
+
+    func decodeJSON<T: Decodable>(data: Data, type: T.Type) -> Result<T, DecodeError>? {
+        do{
+            let decodedBoxOffice: T = try decoder.decode(T.self, from: data)
+            return .success(decodedBoxOffice)
+        } catch {
+            return .failure(.decodingFailureError)
+        }
+    }
+    
+    func decodeJSON<T: Decodable>(fileName: String, type: T.Type) -> Result<T, DecodeError> {
+        
+        guard let JSONFile: NSDataAsset  = NSDataAsset(name: fileName) else {
+            return .failure(.invalidFileError)
+        }
+        
+        do{
+            let decodedJSON: T = try decoder.decode(T.self, from: JSONFile.data)
+            return .success(decodedJSON)
+        } catch {
+            return .failure(.decodingFailureError)
+        }
+    }
+}
