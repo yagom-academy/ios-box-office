42b5cf7b2f69431ecfc0ca8533ada11169da99ef
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2023-03-22 21:04:24 +0900

    feat: loadBoxOfficeAPI 매서드 delegate패턴으로 viewController와 연결 구현

--- BoxOffice/Controller/ViewController.swift
+++ BoxOffice/Controller/ViewController.swift
@@ -7,11 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    var dailyBoxOfficeAPI = DailyBoxOfficeAPI()
+final class ViewController: UIViewController {
+    private var parsedMovieDetail: MovieDetail?
+    private var parsedDailyBoxOffice: DailyBoxOffice?
+    
+    private let dailyBoxOfficeParser = Parser<DailyBoxOffice>()
+    private let movieDetailParser = Parser<MovieDetail>()
+    
+    private var boxOfficeAPI = BoxOfficeAPI()
+    
+    let dailyBoxOfficeURL: String = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101"
+    
+    let movieDetailURL: String = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=f5eef3421c602c6cb7ea224104795888&movieCd=20124079"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        dailyBoxOfficeAPI.loadDailyBoxOffice()
+        boxOfficeAPI.delegate = self
+        
+        boxOfficeAPI.loadBoxOfficeAPI(urlAddress: dailyBoxOfficeURL, parserType: dailyBoxOfficeParser)
+        test()
+    }
+    
+    func test() {
+        Thread.sleep(forTimeInterval: 1)
+        print(parsedDailyBoxOffice?.boxOfficeResult.boxOfficeType)
+    }
+    
+    
+}
+
+extension ViewController: BoxOfficeAPIDelegate {
+    func fetchAPIData<T>(data: T) where T : Decodable {
+    
+        switch data {
+        case is MovieDetail:
+            parsedMovieDetail = data as? MovieDetail
+        case is DailyBoxOffice:
+            parsedDailyBoxOffice = data as? DailyBoxOffice
+        default:
+            return
+        }
     }
 }
--- BoxOffice/Model/BoxOfficeAPI.swift
+++ BoxOffice/Model/BoxOfficeAPI.swift
@@ -7,20 +7,24 @@
 
 import Foundation
 
-struct DailyBoxOfficeAPI {
-    let dailyBoxOfficeParser = Parser<DailyBoxOffice>()
-    let movieDetailParser = Parser<MovieDetail>()
+protocol BoxOfficeAPIDelegate {
+    func fetchAPIData<T: Decodable>(data: T)
+}
+
+struct BoxOfficeAPI {
+  
+    var delegate: BoxOfficeAPIDelegate?
     
-    func loadDailyBoxOffice() {
-        guard let url = URL(string: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=f5eef3421c602c6cb7ea224104795888&movieCd=20124079") else {
+    func loadBoxOfficeAPI<T: Decodable>(urlAddress: String, parserType: Parser<T>) {
+        guard let url = URL(string: urlAddress) else {
             return
         }
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         URLSession.shared.dataTask(with: request) { data, response, error in
             guard let validData = data else { return }
-            print(movieDetailParser.Parse(data: validData)?.movieInformationResult.movieInformation)
-            //print(dailyBoxOfficeParser.Parse(data: validData)?.boxOfficeResult.showRange)
+            guard let parsedData = parserType.Parse(data: validData) else {return}
+            delegate?.fetchAPIData(data: parsedData)
             guard let httpURLResponse = response as? HTTPURLResponse, (200...299).contains(httpURLResponse.statusCode) else {
                 print("response")
                 return
