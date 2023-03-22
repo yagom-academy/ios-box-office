020122c2b4e900a3b5be19524ea712cf766f53c1
minsu3007 <minsu3007@gmail.com>
2023-03-22 16:40:41 +0900

    test: 네트워크에 따른 테스트케이스 추가

--- BoxofficeInfoTests/BoxofficeInfoTests.swift
+++ BoxofficeInfoTests/BoxofficeInfoTests.swift
@@ -14,7 +14,7 @@ final class BoxofficeInfoTests: XCTestCase {
     var mockSession = MockURLSession()
     
     override func setUpWithError() throws {
-        sut = MockBoxofficeInfo(session: mockSession)
+        sut = MockBoxofficeInfo(interfaceValue: "20230320", apiType: .boxoffice, session: mockSession)
     }
     
     override func tearDownWithError() throws {
@@ -54,7 +54,7 @@ final class BoxofficeInfoTests: XCTestCase {
         }
     }
     
-    func test_URL세션을_찾지못하였다() {
+    func test_Session이_실패할경우_responseError를_반환한다() {
         // given
         let expectation = BoxofficeError.responseError
         
@@ -70,7 +70,30 @@ final class BoxofficeInfoTests: XCTestCase {
                 XCTAssertEqual(result, expectation)
             }
         }
+    }
+    
+    func test_네트워크가_연결이되어있을경우_dailyBoxOfficeList의_요소갯수가_10개이고_안되어있을경우_sessionerror를_반환한다() {
+        // given
+        let asyncTest = XCTestExpectation()
         
+        let errorExpectation = BoxofficeError.sessionError
+        let successExpectation = 10
+        // when
+        sut.session = URLSession.shared
+        
+        // then
+        sut.search { event in
+            switch event {
+            case .success(let data):
+                let result = data.boxOfficeResult.movies.count
+                XCTAssertEqual(result, successExpectation)
+            case .failure(let error):
+                XCTAssertEqual(error, errorExpectation)
+            }
+            asyncTest.fulfill()
+        }
+        
+        wait(for: [asyncTest], timeout: 10.0)
     }
     
     
--- BoxofficeInfoTests/MockBoxofficeInfo.swift
+++ BoxofficeInfoTests/MockBoxofficeInfo.swift
@@ -9,36 +9,45 @@ import Foundation
 @testable import BoxOffice
 
 class MockBoxofficeInfo<T: Fetchable> {
-    let session: URLSessionProtocol
+    let interfaceValue: String
+    let apiType: APIType
+    var session: URLSessionProtocol
     
-    init(session: URLSessionProtocol = URLSession.shared) {
+    init(interfaceValue: String, apiType: APIType, session: URLSessionProtocol) {
+        self.interfaceValue = interfaceValue
+        self.apiType = apiType
         self.session = session
     }
     
     func search(completion: @escaping (Result<T, BoxofficeError>) -> Void) {
-        let url = URL(string: "https://www.naver.com")!
-        
-        let task = session.dataTask(with: url) { data, response, error in
-            if error != nil {
-                completion(.failure(.sessionError))
-                return
-            }
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode) else {
-                completion(.failure(.responseError))
-                return
-            }
-            if let data = data {
-                do {
-                    let decodingdata = try JSONDecoder().decode(T.self, from: data)
-                    completion(.success(decodingdata))
-                } catch {
-                    completion(.failure(.decodingError))
+        do {
+            let url = try apiType.getUrl(interfaceValue: interfaceValue)
+            
+            let task = session.dataTask(with: url) { data, response, error in
+                
+                if error != nil {
+                    completion(.failure(.sessionError))
                     return
                 }
+                guard let httpResponse = response as? HTTPURLResponse,
+                      (200...299).contains(httpResponse.statusCode) else {
+                    completion(.failure(.responseError))
+                    return
+                }
+                if let data = data {
+                    do {
+                        let decodingdata = try JSONDecoder().decode(T.self, from: data)
+                        completion(.success(decodingdata))
+                    } catch {
+                        completion(.failure(.decodingError))
+                        return
+                    }
+                }
             }
+            
+            task.resume()
+        } catch {
+            completion(.failure(.urlError))
         }
-        
-        task.resume()
     }
 }
