e31f0b48b11bfe71276151e945359d2e0a98314c
parkmuri <smxhmu@naver.com>
2023-03-22 18:35:05 +0900

    refactor: DataManagerTests 작성

--- BoxOfficeTests/DataManagerTests/DataManagerTests.swift
+++ BoxOfficeTests/DataManagerTests/DataManagerTests.swift
@@ -12,23 +12,28 @@ final class DataManagerTests: XCTestCase {
     var sut: DataManager!
     
     override func setUpWithError() throws {
-        sut = DataManager()
+        sut = DataManager(kobisUrlSession: MockURLSession())
     }
     
     override func tearDownWithError() throws {
         sut = nil
     }
     
-    func test_startLoadDailyBoxOfficeData호출_실패시_() {
+    func test_startLoadDailyBoxOfficeData호출시_date로20230101을받을때_네트워킹이_성공한경우() {
         // given
-        
+        let expectation = XCTestExpectation()
+        let expectedResult = try? JSONDecoder().decode(DailyBoxOffice.self, from: KobisAPI.dailyBoxOffice.sampleData)
         
         // when
-        
-        
-        // then
-        
-
+        sut.startLoadDailyBoxOfficeData(date: "20230101") { result in
+            switch result {
+                // then
+            case .success(let dailyBoxOffice):
+                XCTAssertEqual(dailyBoxOffice.boxOfficeResult.boxOfficeType, expectedResult?.boxOfficeResult.boxOfficeType)
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+        }
     }
     
     func test_startLoadDailyBoxOfficeData호출_성공시_예상한boxOfficType과showRange와_일치한다() {
--- BoxOfficeTests/DataManagerTests/KobisAPI.swift
+++ BoxOfficeTests/DataManagerTests/KobisAPI.swift
@@ -9,17 +9,39 @@ import UIKit
 
 enum KobisAPI {
     case dailyBoxOffice
+    case movieDetails
     
-    static let baseURL = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
-    static let queryParameters = "?key=d975f8608af0d9e5a16e79768ca97127&targetDt=20230101"
+    var baseURL: String {
+        switch self {
+        case .dailyBoxOffice:
+            return "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
+        case .movieDetails:
+            return "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json"
+        }
+    }
+    
+    var queryParameters: String {
+        switch self {
+        case .dailyBoxOffice:
+            return "?key=d975f8608af0d9e5a16e79768ca97127&targetDt=20230101"
+        case .movieDetails:
+            return "?key=d975f8608af0d9e5a16e79768ca97127&movieCd=20199882"
+        }
+    }
     
     var url: URL? {
-        guard let url = URL(string: Self.baseURL + Self.queryParameters) else { return nil }
+        guard let url = URL(string: self.baseURL + self.queryParameters) else { return nil }
         return url
     }
     
     var sampleData: Data {
-        let sampleData = NSDataAsset(name: "DailyBoxOffice")!.data
-        return sampleData
+        switch self {
+        case .dailyBoxOffice:
+            let sampleData = NSDataAsset(name: "DailyBoxOffice")!.data
+            return sampleData
+        case .movieDetails:
+            let sampleData = NSDataAsset(name: "ThePolicemansLineage")!.data
+            return sampleData
+        }
     }
 }
