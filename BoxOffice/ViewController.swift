fe1b91828708b800585aaac33b1014e7167614e9
minsu3007 <minsu3007@gmail.com>
2023-03-22 17:20:59 +0900

    chore: 코드 정리

--- BoxOffice/ViewController.swift
+++ BoxOffice/ViewController.swift
@@ -9,19 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var movie = BoxofficeInfo<MovieInfoObject>(interfaceValue: "20228555", apiType: .movie)
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        movie.search { result in
-            switch result {
-            case .success(let data):
-                print(data)
-            case .failure(let error):
-                print(error)
-            }
-        }
     }
 
 
