415d7d9771e845103918f5186416af4bbb9e6d6d
sehong <tjsgh0801@naver.com>
2023-03-23 17:08:33 +0900

    feat: 파라미터 정렬을 위한 URL Extension 추가

---
+++ BoxOffice/Extensions/URL+.swift
@@ -0,0 +1,22 @@
+//
+//  URL+.swift
+//  BoxOffice
+//
+//  Created by Seoyeon Hong on 2023/03/23.
+//
+
+import Foundation
+
+extension URL {
+    var normalizedURL: URL? {
+        guard var components = URLComponents(url: self, resolvingAgainstBaseURL: false) else {
+            return nil
+        }
+        
+        if let queryItems = components.queryItems {
+            components.queryItems = queryItems.sorted { $0.name < $1.name }
+        }
+        
+        return components.url
+    }
+}
