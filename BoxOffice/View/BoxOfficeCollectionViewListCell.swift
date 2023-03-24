102e53c4666ebe601620cfcad538c94bde65da79
HarryHyeon <hharry0728@gmail.com>
2023-03-24 15:55:10 +0900

    feat: BoxOfficeCollectionListCell 기본 구현

---
+++ BoxOffice/View/BoxOfficeCollectionViewListCell.swift
@@ -0,0 +1,17 @@
+//
+//  BoxOfficeCollectionViewListCell.swift
+//  BoxOffice
+//
+//  Created by Harry on 2023/03/24.
+//
+
+import UIKit
+
+class BoxOfficeCollectionViewListCell: UICollectionViewListCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
