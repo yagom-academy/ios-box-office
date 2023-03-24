9691e0083ac3de79477d77e3b4153cb9515dda60
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2023-03-24 11:01:12 +0900

    refacor: loadBoxOfficeAPI data, response, error 구조 및 순서 변경

--- BoxOffice/Model/BoxOfficeAPI.swift
+++ BoxOffice/Model/BoxOfficeAPI.swift
@@ -22,13 +22,17 @@ struct BoxOfficeAPI {
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let validData = data else { return }
-            guard let parsedData = parserType.Parse(data: validData) else {return}
-            delegate?.fetchAPIData(data: parsedData)
+            guard error != nil else { return }
+            
             guard let httpURLResponse = response as? HTTPURLResponse, (200...299).contains(httpURLResponse.statusCode) else {
                 print("response")
                 return
             }
+            
+            guard let validData = data, let parsedData = parserType.Parse(data: validData) else { return }
+
+            delegate?.fetchAPIData(data: parsedData)
+           
         }.resume()
     }
 }
