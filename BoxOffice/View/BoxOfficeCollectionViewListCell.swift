3c40daf23ff05e6cc64a8b297aef6a3ebb866bb1
HarryHyeon <hharry0728@gmail.com>
2023-03-27 17:45:42 +0900

    feat: BoxOfficeCollectionViewListCell 구현

--- BoxOffice/View/BoxOfficeCollectionViewListCell.swift
+++ BoxOffice/View/BoxOfficeCollectionViewListCell.swift
@@ -7,11 +7,34 @@
 
 import UIKit
 
-class BoxOfficeCollectionViewListCell: UICollectionViewListCell {
-
+final class BoxOfficeCollectionViewListCell: UICollectionViewListCell {
+    @IBOutlet private weak var rankLabel: UILabel!
+    @IBOutlet private weak var rankInfoLabel: UILabel!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var audienceInfoLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        configureLabels()
     }
-
+    
+    private func configureLabels() {
+        rankLabel.font = .preferredFont(forTextStyle: .largeTitle)
+        rankInfoLabel.font = .preferredFont(forTextStyle: .subheadline)
+        titleLabel.font = .preferredFont(forTextStyle: .title3)
+        audienceInfoLabel.font = .preferredFont(forTextStyle: .body)
+    }
+    
+    func configureCellContent(item: DailyBoxOffice) {
+        rankLabel.text = item.rankText
+        
+//        if item.rankOldAndNew == .new {
+//            rankInfoLabel.text = "신작"
+//        } else if Int(item.rankIncrementText) < 0 {
+//            rankInfoLabel.text = item.rankIncrementText
+//        }
+        rankInfoLabel.text = item.rankIncrementText
+        titleLabel.text = item.movieKoreanName
+        audienceInfoLabel.text = "오늘 \(item.audienceCountText) / 총 \(item.audienceAccumulationText)"
+    }
 }
