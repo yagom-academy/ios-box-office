2ebd9f9571e3b803ac4cc2f5dbdbf4e0c1791128
vetto <gzzjk159@naver.com>
2023-03-27 21:57:42 +0900

    feat: BoxOfficeContentView 정의

--- BoxOffice/Model/BoxOfficeContentView.swift
+++ BoxOffice/Model/BoxOfficeContentView.swift
@@ -8,7 +8,27 @@
 import UIKit
 
 class BoxOfficeContentView: UIView, UIContentView {
-    var configuration: UIContentConfiguration
+    private var currentConfiguration: BoxOfficeContentConfiguration!
+    var configuration: UIContentConfiguration {
+        get {
+            return currentConfiguration
+        }
+        set {
+            guard let newConfiguration = newValue as? BoxOfficeContentConfiguration else {
+                return
+            }
+            
+            apply(configuration: newConfiguration)
+        }
+    }
+    
+    let rankLabel = UILabel()
+    let rankStackView = UIStackView()
+    let rankIncrementSymbol = UIImageView()
+    let rankIncrementLabel = UILabel()
+    let newRankLabel = UILabel()
+    let titleLabel = UILabel()
+    let audienceCountLabel = UILabel()
     
     init(configuration: BoxOfficeContentConfiguration) {
         
