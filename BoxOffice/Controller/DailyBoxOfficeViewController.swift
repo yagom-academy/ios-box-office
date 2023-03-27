8de84788d7f675e3bf059798a7fb3f142a735f88
parkmuri <smxhmu@naver.com>
2023-03-27 18:20:41 +0900

    refactor: DailyBoxOfficeCell 및 DailyBoxOfficeViewController 수정

--- BoxOffice/Controller/DailyBoxOfficeViewController.swift
+++ BoxOffice/Controller/DailyBoxOfficeViewController.swift
@@ -12,7 +12,7 @@ final class DailyBoxOfficeViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     
     var dailyBoxOffice: DailyBoxOffice?
-    var dataSource: UICollectionViewDiffableDataSource<Section, DailyBoxOffice>!
+    var dataSource: UICollectionViewDiffableDataSource<Section, DailyBoxOfficeMovie>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,13 +21,21 @@ final class DailyBoxOfficeViewController: UIViewController {
     }
     
     func configureDataSource() {
-        let cellRegistration = UICollectionView.CellRegistration<DailyBoxOfficeCell, DailyBoxOffice> { (cell, indexPath, item) in
-            cell.contentView
-            cell.rankLabel
+        let cellRegistration = UICollectionView.CellRegistration<DailyBoxOfficeCell, DailyBoxOfficeMovie> { (cell, indexPath, item) in
             
-            let row = indexPath
-            
+            cell.update(with: item)
+            cell.accessories = [.disclosureIndicator()]
         }
+        
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView) {
+            (collectionView, indexPath, itemIdentifier) -> UICollectionViewCell? in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, DailyBoxOfficeMovie>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems((dailyBoxOffice?.boxOfficeResult.dailyBoxOfficeList)!)
+        dataSource.apply(snapshot)
     }
     
     func creatList() -> UICollectionViewCompositionalLayout {
@@ -38,13 +46,18 @@ final class DailyBoxOfficeViewController: UIViewController {
     }
     
     private func loadDailyBoxOffice() {
-        let api = KobisAPI(service: .dailyBoxOffice)
+        var api = KobisAPI(service: .dailyBoxOffice)
+        api.addQuery(name: "targetDt", value: "20230101")
         var apiProvider = APIProvider()
         apiProvider.target(api: api)
         apiProvider.startLoad(decodingType: DailyBoxOffice.self) { result in
             switch result {
             case .success(let dailyBoxOffice):
                 self.dailyBoxOffice = dailyBoxOffice
+                print(dailyBoxOffice)
+                DispatchQueue.main.async {
+                    self.configureDataSource()
+                }
             case .failure(let error):
                 print(error.localizedDescription)
             }
--- BoxOffice/View/DailyBoxOfficeCell.swift
+++ BoxOffice/View/DailyBoxOfficeCell.swift
@@ -7,12 +7,27 @@
 
 import UIKit
 
-@available(iOS 14.0, *)
 final class DailyBoxOfficeCell: UICollectionViewListCell, Identifiable {
+    var dailyBoxOfficeData: DailyBoxOfficeMovie?
     var rankLabel = UILabel()
     var rankDifferenceLabel = UILabel()
-    var dailyBoxOfficeContentView = UIListContentView(configuration: .subtitleCell())
+//    var dailyBoxOfficeContentView = UIListContentView(configuration: .subtitleCell())
     var isConstraintNeeded = true
+    var dailyBoxOfficeListContentView = UIListContentView(configuration: UIListContentConfiguration.subtitleCell())
+    
+    func update(with newDailyBoxOfficeData: DailyBoxOfficeMovie) {
+        guard dailyBoxOfficeData != newDailyBoxOfficeData else { return }
+        
+        dailyBoxOfficeData = newDailyBoxOfficeData
+        setLayoutConstraint()
+    }
+    
+    override var configurationState: UICellConfigurationState {
+        var state = super.configurationState
+        state.dailyBoxOfficeData = self.dailyBoxOfficeData
+        
+        return state
+    }
     
     func setLayoutConstraint() {
         guard self.isConstraintNeeded else { return }
@@ -26,20 +41,44 @@ final class DailyBoxOfficeCell: UICollectionViewListCell, Identifiable {
             return stackView
         }()
         
-        [dailyBoxOfficeContentView, rankStackView].forEach {
+        [dailyBoxOfficeListContentView, rankStackView].forEach {
             self.contentView.addSubview($0)
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
         
         NSLayoutConstraint.activate([
             rankStackView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
-            rankStackView.trailingAnchor.constraint(equalTo: dailyBoxOfficeContentView.leadingAnchor),
+            rankStackView.trailingAnchor.constraint(equalTo: dailyBoxOfficeListContentView.leadingAnchor),
             rankStackView.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
-            dailyBoxOfficeContentView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
-            dailyBoxOfficeContentView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
-            dailyBoxOfficeContentView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor)
+            dailyBoxOfficeListContentView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            dailyBoxOfficeListContentView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+            dailyBoxOfficeListContentView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor)
         ])
         
         self.isConstraintNeeded = false
+    }
+    
+    override func updateConfiguration(using state: UICellConfigurationState) {
+        setLayoutConstraint()
+        
+        var content = defaultContentConfiguration().updated(for: state)
+        content.text = state.dailyBoxOfficeData?.movieName
+        content.secondaryText = "\(state.dailyBoxOfficeData?.audienceCountOfDate), \(state.dailyBoxOfficeData?.accumulatedAudienceCount)"
+        
+        dailyBoxOfficeListContentView.configuration = content
+        
+        rankLabel.text = state.dailyBoxOfficeData?.rank ?? "0"
+        rankDifferenceLabel.text = state.dailyBoxOfficeData?.rankDifference ?? "0"
+    }
+}
+
+private extension UIConfigurationStateCustomKey {
+    static let dailyBoxOffice = UIConfigurationStateCustomKey("dailyBoxOffice")
+}
+
+extension UIConfigurationState {
+    var dailyBoxOfficeData: DailyBoxOfficeMovie? {
+        get { return self[.dailyBoxOffice] as? DailyBoxOfficeMovie }
+        set { self[.dailyBoxOffice] = newValue }
     }
 }
