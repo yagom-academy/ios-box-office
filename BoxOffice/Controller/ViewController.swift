02ab4b3973160b2e180752397a3c9e2a70512ca1
ROWAN <kimkj93@naver.com>
2023-03-27 14:30:21 +0900

    feat: collectionView IBOutlet 생성 및 loadDailyBoxOffice 메서드 정의

--- BoxOffice/Controller/ViewController.swift
+++ BoxOffice/Controller/ViewController.swift
@@ -7,9 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class DailyBoxOfficeViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var dailyBoxOffice: DailyBoxOffice?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        self.loadDailyBoxOffice()
     }
+    
+    private func loadDailyBoxOffice() {
+        let api = KobisAPI(service: .dailyBoxOffice)
+        var apiProvider = APIProvider()
+        apiProvider.target(api: api)
+        apiProvider.startLoad(decodingType: DailyBoxOffice.self) { result in
+            switch result {
+            case .success(let dailyBoxOffice):
+                self.dailyBoxOffice = dailyBoxOffice
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+extension DailyBoxOfficeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
+
+extension DailyBoxOfficeViewController: UICollectionViewDelegate {
+    
 }
