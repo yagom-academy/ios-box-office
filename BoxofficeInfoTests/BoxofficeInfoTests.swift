ad044d0f50223bb35d701766008e06eb6be23c66
minsu3007 <minsu3007@gmail.com>
2023-03-28 20:26:55 +0900

    test: BoxofficeInfoTests 실패 URLResponse 케이스 추가

--- BoxofficeInfoTests/BoxofficeInfoTests.swift
+++ BoxofficeInfoTests/BoxofficeInfoTests.swift
@@ -9,15 +9,15 @@ import XCTest
 @testable import BoxOffice
 
 final class MockNetworkModel: NetworkingProtocol {
+    private var callCount = 0
+    private var lastData: Data?
+    private var lastResponse: HTTPURLResponse?
+    private var lastError: BoxofficeError?
+    private var session: URLSession
+    
     init(session: URLSession) {
         self.session = session
     }
-    
-    var callCount = 0
-    var lastData: Data?
-    var lastResponse: HTTPURLResponse?
-    var lastError: Error?
-    var session: URLSession
     
     func search(url: URL, completion: @escaping (Result<Data, BoxOffice.BoxofficeError>) -> Void) -> URLSessionDataTask {
         callCount += 1
@@ -26,6 +26,7 @@ final class MockNetworkModel: NetworkingProtocol {
             guard let httpResponse = response as? HTTPURLResponse,
                   (200...299).contains(httpResponse.statusCode) else {
                 self.lastResponse = response as? HTTPURLResponse
+                self.lastError = .responseError
                 completion(.failure(.responseError))
                 return
             }
@@ -49,13 +50,21 @@ final class MockNetworkModel: NetworkingProtocol {
         XCTAssertEqual(self.lastResponse?.url, httpResponse.url)
         XCTAssertEqual(self.callCount, callCount)
     }
+    
+    func validateFailureTask(httpResponse: HTTPURLResponse, callCount: Int, error: BoxofficeError?) {
+        XCTAssertNil(self.lastData)
+        XCTAssertEqual(self.lastResponse?.statusCode, httpResponse.statusCode)
+        XCTAssertEqual(self.lastResponse?.url, httpResponse.url)
+        XCTAssertEqual(self.callCount, callCount)
+        XCTAssertEqual(self.lastError, error)
+    }
 }
 
 final class BoxofficeInfoTests: XCTestCase {
     
-    var sut: BoxofficeInfo<DailyBoxofficeObject>!
-    var model: MockNetworkModel!
-    var url = APIType.boxoffice("20230322").receiveUrl()!
+    private var sut: BoxofficeInfo<DailyBoxofficeObject>!
+    private var model: MockNetworkModel!
+    private var url = APIType.boxoffice("20230322").receiveUrl()!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -73,7 +82,7 @@ final class BoxofficeInfoTests: XCTestCase {
         model = nil
     }
     
-    func test_URLResponse가_200번대라면_Data타입을_받아온다() {
+    func test_성공한_URLResponse를_가질때_validateSuccessTask가_성공한다() {
         // given
         MockURLProtocolObject.requestHandler = { request in
             guard let url = request.url,
@@ -83,6 +92,7 @@ final class BoxofficeInfoTests: XCTestCase {
             
             let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: "2.0", headerFields: nil)!
             let data = StubBoxoffice().data
+            
             return (response, data)
         }
         
@@ -90,8 +100,13 @@ final class BoxofficeInfoTests: XCTestCase {
         let asyncTest = XCTestExpectation()
         
         // when
-        sut.fetchData { _ in
-            asyncTest.fulfill()
+        sut.fetchData { event in
+            switch event {
+            case .success(_):
+                asyncTest.fulfill()
+            case .failure(_):
+                XCTFail("잘못된 테스트코드입니다.")
+            }
         }
 
         wait(for: [asyncTest], timeout: 3)
@@ -100,5 +115,37 @@ final class BoxofficeInfoTests: XCTestCase {
         model.vaildateSuccessTask(data: StubBoxoffice().data, httpResponse: response, callCount: 1)
     }
     
-    
+    func test_실패한_URLResponse를_가질때_validateFailureTask가_성공한다() {
+        // given
+        MockURLProtocolObject.requestHandler = { request in
+            guard let url = request.url,
+                  url == self.url else {
+                throw BoxofficeError.urlError
+            }
+            
+            let response = HTTPURLResponse(url: url, statusCode: 403, httpVersion: "2.0", headerFields: nil)!
+            let data = Data()
+            
+            return (response, data)
+        }
+        
+        var expectationError: BoxofficeError?
+        let response = HTTPURLResponse(url: url, statusCode: 403, httpVersion: "2.0", headerFields: nil)!
+        let asyncTest = XCTestExpectation()
+        
+        // when
+        sut.fetchData { event in
+            switch event {
+            case .success(_):
+                XCTFail("잘못된 테스트코드입니다.")
+            case .failure(let error):
+                expectationError = error
+                asyncTest.fulfill()
+            }
+        }
+        
+        wait(for: [asyncTest], timeout: 3)
+        
+        model.validateFailureTask(httpResponse: response, callCount: 1, error: expectationError)
+    }
 }
