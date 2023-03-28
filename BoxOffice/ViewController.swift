b9cfc02e4000e83764901c7738f50492a2c2469a
sehong <tjsgh0801@naver.com>
2023-03-28 12:03:33 +0900

    feat: collectionview를 그려주는  함수 및 extension UICollectionViewDelegate  구현

--- BoxOffice/ViewController.swift
+++ BoxOffice/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 final class ViewController: UIViewController {
     
+    private var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<ListSection, ListItem>!
+    
     let provider = APIProvider.shared
     
     override func viewDidLoad() {
@@ -45,5 +48,34 @@ final class ViewController: UIViewController {
         guard let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: today) else { return nil }
         return dateFormatter.string(from: yesterday)
     }
+    
+    // MARK: - CollectionView
+    private func createLayout() -> UICollectionViewLayout {
+        let config = UICollectionLayoutListConfiguration(appearance: .plain)
+        return UICollectionViewCompositionalLayout.list(using: config)
+    }
+    
+    private func configureHierarchy() {
+        let layout = createLayout()
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+        
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+    }
+    
+    private func configureViewController() {
+        guard let yesterday = createFormattedDate(dateFormat: "yyyy-MM-dd") else { return }
+        navigationController?.navigationBar.topItem?.title = yesterday
+        navigationController?.navigationBar.titleTextAttributes = [.font: UIFont.systemFont(ofSize: 18, weight: .bold)]
+    }
 
 }
+
+// MARK: - UICollectionViewDelegate
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+}
+
