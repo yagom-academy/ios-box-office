6049533d6f70e7931167c238969b606805b944d4
kaki <ycg02109@naver.com>
2023-03-28 10:33:07 +0900

    feat: configureRankInfoLabel, attributedString 구현

--- BoxOffice/View/BoxOfficeCollectionViewListCell.swift
+++ BoxOffice/View/BoxOfficeCollectionViewListCell.swift
@@ -15,7 +15,14 @@ final class BoxOfficeCollectionViewListCell: UICollectionViewListCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        
         configureLabels()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        rankInfoLabel.textColor = .black
     }
     
     private func configureLabels() {
@@ -27,14 +34,40 @@ final class BoxOfficeCollectionViewListCell: UICollectionViewListCell {
     
     func configureCellContent(item: DailyBoxOffice) {
         rankLabel.text = item.rankText
-        
-//        if item.rankOldAndNew == .new {
-//            rankInfoLabel.text = "신작"
-//        } else if Int(item.rankIncrementText) < 0 {
-//            rankInfoLabel.text = item.rankIncrementText
-//        }
-        rankInfoLabel.text = item.rankIncrementText
+        configureRankInfoLabel(item: item)
         titleLabel.text = item.movieKoreanName
         audienceInfoLabel.text = "오늘 \(item.audienceCountText.convertToDecimalText()) / 총 \(item.audienceAccumulationText.convertToDecimalText())"
+    }
+    
+    private func configureRankInfoLabel(item: DailyBoxOffice) {
+        switch item.rankOldAndNew {
+        case .new:
+            rankInfoLabel.text = "신작"
+            rankInfoLabel.textColor = .systemRed
+        case .old:
+            guard let increment = Int(item.rankIncrementText) else { return }
+            
+            if increment > 0 {
+                rankInfoLabel.textColor = .systemRed
+                rankInfoLabel.attributedText = "▲\(increment)".attributedString
+            } else if increment < 0 {
+                rankInfoLabel.textColor = .systemBlue
+                rankInfoLabel.attributedText = "▼\(increment * -1)".attributedString
+            } else {
+                rankInfoLabel.text = "-"
+            }
+        }
+    }
+}
+
+fileprivate extension String {
+    var attributedString: NSMutableAttributedString {
+        let attributedString = NSMutableAttributedString(string: self)
+        let index = self.index(after: self.startIndex)
+        
+        let range = String(self[index...])
+        attributedString.addAttribute(.foregroundColor, value: UIColor.black, range: (self as NSString).range(of: range))
+        
+        return attributedString
     }
 }
