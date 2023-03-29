4b53ba6ef47946a0c3cc2b1e9cb072f8d9b3e683
kaki <ycg02109@naver.com>
2023-03-29 15:55:53 +0900

    feat: UIImageView 확장 구현, 영화 정보, 포스터 정보 fetch 메서드 구현

--- BoxOffice/Controller/MovieInfoViewController.swift
+++ BoxOffice/Controller/MovieInfoViewController.swift
@@ -8,13 +8,18 @@
 import UIKit
 
 final class MovieInfoViewController: UIViewController {
+    @IBOutlet private weak var posterImageView: UIImageView!
+    
     private let movieCode: String?
     private let movieName: String?
-
+    private let networkManager = NetworkManager()
+    private var movieInfo: Movie?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = movieName
+        fetchMoviePoster()
     }
     
     init?(movieCode: String?, movieName: String?, coder: NSCoder) {
@@ -27,4 +32,37 @@ final class MovieInfoViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func fetchMovieInfo() {
+        guard let movieCode = movieCode else { return }
+        let endPoint: BoxOfficeEndPoint = .fetchMovieInfo(movieCode: movieCode)
+        
+        networkManager.fetchData(request: endPoint.createRequest(), type: Movie.self) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let data):
+                    self.movieInfo = data
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    private func fetchMoviePoster() {
+        guard let movieName = movieName else { return }
+        let endPoint: BoxOfficeEndPoint = .fetchMoviePoster(movieName: movieName)
+        
+        networkManager.fetchData(request: endPoint.createRequest(), type: MoviePoster.self) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let data):
+                    guard let urlText = data.items.first?.imageURLText else { return }
+                    let url = URL(string: urlText)
+                    self.posterImageView.load(url: url)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
 }
