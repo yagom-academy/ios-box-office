00a50fcf2a33ecd739b64466e227d9da08071fbe
kimseongj <ksj8277@naver.com>
2023-03-29 11:16:48 +0900

    fear : Provider 구현

--- BoxOffice/Network/Provider.swift
+++ BoxOffice/Network/Provider.swift
@@ -2,7 +2,22 @@
 //  Provider.swift
 //  BoxOffice
 //
-//  Created by kimseongjun on 2023/03/28.
+//  Created by goat, songjun on 2023/03/28.
 //
 
 import Foundation
+
+struct Provider {
+    func loadBoxOfficeAPI<T: Decodable>(endpoint: EndPoint, parser: Parser<T>, completion: @escaping (T) -> Void) {
+        guard let request = endpoint.makeURLRequest() else { return }
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else { return }
+            
+            guard let httpURLResponse = response as? HTTPURLResponse, (200...299).contains(httpURLResponse.statusCode) else { return }
+            
+            guard let validData = data, let parsedData = parser.parse(data: validData) else { return }
+            completion(parsedData)
+        }.resume()
+    }
+}
