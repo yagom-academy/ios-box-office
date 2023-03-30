8cdc40ced4400aa3039805a66f3664039d25ea13
Rhode-park <jinah.park.16@gmail.com>
2023-03-30 21:50:58 +0900

    feat: 유닛테스트를 위한 작업들

--- NetworkManagerTests/NetworkManagerTests.swift
+++ NetworkManagerTests/NetworkManagerTests.swift
@@ -10,16 +10,32 @@ import XCTest
 
 final class NetworkManagerTests: XCTestCase {
     var sut: NetworkManager!
-
+    
     override func setUpWithError() throws {
-        sut = NetworkManager(session: URLSession())
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [MockURLProtocolObject.self]
+        let session = URLSession(configuration: configuration)
+        
+        sut = NetworkManager(session: session)
     }
-
+    
     override func tearDownWithError() throws {
         sut = nil
     }
     
     func test_() {
-      
+        //given
+        MockURLProtocolObject.requestHandler = { request in
+            let data = StubBoxOffice().data
+            let responses = HTTPURLResponse(url: request.url!, mimeType: "text", expectedContentLength: 500, textEncodingName: nil)
+            
+            return (responses, data)
+        }
+        
+        
+        //when
+        
+        //then
+        
     }
 }
