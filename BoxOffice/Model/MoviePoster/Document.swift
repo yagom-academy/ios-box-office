0f118b12b4223aaf94c3814fe304cbdb163549cb
Jun <59365211+juun97@users.noreply.github.com>
2023-04-01 01:24:27 +0900

    feat : JSON 모델 구현

---
+++ BoxOffice/Model/MoviePoster/Document.swift
@@ -0,0 +1,13 @@
+//
+//  Document.swift
+//  BoxOffice
+//
+//  Created by Rhode, Rilla on 2023/04/01.
+//
+
+struct Document: Decodable {
+    let contents: String
+    let datetime: String
+    let title: String
+    let url: String
+}
--- BoxOffice/Network/URLMaker.swift
+++ BoxOffice/Network/URLMaker.swift
@@ -8,11 +8,12 @@
 import Foundation
 
 struct URLMaker {
-    private let key = "d1fb8a58834af4265bbe3cb487e9a994"
+    private let kobisAPIKey = "d1fb8a58834af4265bbe3cb487e9a994"
+    private let kakaoAPIKey = "c81ecfbda2df87ff1ed83ba3bd2f6c71"
     
     func makeBoxOfficeURL(date: String) -> URL? {
         var components = URLComponents(string: "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json")
-        let keyItem = URLQueryItem(name: "key", value: key)
+        let keyItem = URLQueryItem(name: "key", value: kobisAPIKey)
         let targetDateItem = URLQueryItem(name: "targetDt", value: date)
         
         components?.queryItems = [keyItem, targetDateItem]
@@ -22,11 +23,17 @@ struct URLMaker {
     
     func makeMovieInformationURL(movieCode: String) -> URL? {
         var components = URLComponents(string: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json")
-        let keyItem = URLQueryItem(name: "key", value: key)
+        let keyItem = URLQueryItem(name: "key", value: kobisAPIKey)
         let movieCodeItem = URLQueryItem(name: "movieCd", value: movieCode)
         
         components?.queryItems = [keyItem, movieCodeItem]
         
         return components?.url
+    }
+    
+    func makeMoviePosterURL(movieName: String) -> URL? {
+        var components = URLComponents(string: "https://dapi.kakao.com/v2/search/image")
+        let keyItem = URLQueryItem(name: "key", value: kakaoAPIKey)
+        let movieNameForSearch = "\(movieName) 영화포스터"
     }
 }
