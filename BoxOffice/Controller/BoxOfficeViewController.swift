d248ea80f01854311c3cc473b2f6c7332966cc49
kimseongj <ksj8277@naver.com>
2023-03-31 11:50:44 +0900

    refactor: BoxOfficeListCell 내부에 함수를 선언해서 BoxOfficeViewController에 메서드를 불러와 데이터를 전달할 수 있도록 구현

--- BoxOffice/Controller/BoxOfficeViewController.swift
+++ BoxOffice/Controller/BoxOfficeViewController.swift
@@ -102,18 +102,14 @@ extension BoxOfficeViewController: UICollectionViewDataSource {
         let cellId = String(describing: BoxOfficeListCell.self)
         let cell = boxOfficeListCollectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! BoxOfficeListCell
         
-        guard let audienceCount = dailyBoxOffice?.boxOfficeResult.dailyBoxOfficeList[indexPath.row].audienceCount else { return cell }
-        guard let audienceAccumulation = dailyBoxOffice?.boxOfficeResult.dailyBoxOfficeList[indexPath.row].audienceAccumulation else { return cell }
+        guard let validDailyBoxOffice = dailyBoxOffice else {
+            return cell
+        }
         
         cell.setUpBoxOffcieCellUI()
-        
+        cell.setUpLabel(by: validDailyBoxOffice, indexPath: indexPath)
         cell.accessories = [.disclosureIndicator() ]
-        
-        cell.rankNumberLabel.text = dailyBoxOffice?.boxOfficeResult.dailyBoxOfficeList[indexPath.row].rank
-        cell.rankGapLabel.attributedText = convertRankGapPresentation(indexPath: indexPath)
-        cell.movieTitleLabel.text = dailyBoxOffice?.boxOfficeResult.dailyBoxOfficeList[indexPath.row].movieName
-        cell.audienceCountLabel.text = "오늘 " + audienceCount + " / 총 " + audienceAccumulation
-        
+    
         return cell
     }
     
--- BoxOffice/View/BoxOfficeListCell.swift
+++ BoxOffice/View/BoxOfficeListCell.swift
@@ -23,7 +23,7 @@ class BoxOfficeListCell: UICollectionViewListCell  {
         
         return stackView
     }()
- 
+    
     private let titleAndAudienceStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -46,7 +46,7 @@ class BoxOfficeListCell: UICollectionViewListCell  {
         
         self.addSubview(rankStackView)
         self.addSubview(titleAndAudienceStackView)
-                
+        
         rankStackView.addArrangedSubview(rankNumberLabel)
         rankStackView.addArrangedSubview(rankGapLabel)
         
@@ -56,11 +56,11 @@ class BoxOfficeListCell: UICollectionViewListCell  {
         NSLayoutConstraint.activate([
             rankStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
             rankStackView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-          
+            
             titleAndAudienceStackView.leadingAnchor.constraint(equalTo: rankStackView.trailingAnchor,constant: 20),
             titleAndAudienceStackView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             titleAndAudienceStackView.trailingAnchor.constraint(lessThanOrEqualTo: self.trailingAnchor, constant: -20)
-            ])
+        ])
     }
     
     private func setUpLabelStyle() {
@@ -68,5 +68,36 @@ class BoxOfficeListCell: UICollectionViewListCell  {
         rankGapLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
         movieTitleLabel.font = UIFont.preferredFont(forTextStyle: .title3)
         audienceCountLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
+    }
+    
+    func setUpLabel(by dailyBoxOffice: DailyBoxOffice, indexPath: IndexPath) {
+        let audienceCount = dailyBoxOffice.boxOfficeResult.dailyBoxOfficeList[indexPath.row].audienceCount
+        let audienceAccumulation = dailyBoxOffice.boxOfficeResult.dailyBoxOfficeList[indexPath.row].audienceAccumulation
+        
+        self.rankNumberLabel.text = dailyBoxOffice.boxOfficeResult.dailyBoxOfficeList[indexPath.row].rank
+        self.rankGapLabel.attributedText = convertRankGapPresentation(dailyBoxOffice: dailyBoxOffice, indexPath: indexPath)
+        self.movieTitleLabel.text = dailyBoxOffice.boxOfficeResult.dailyBoxOfficeList[indexPath.row].movieName
+        self.audienceCountLabel.text = "오늘 " + audienceCount + " / 총 " + audienceAccumulation
+    }
+    
+    private func convertRankGapPresentation(dailyBoxOffice: DailyBoxOffice, indexPath: IndexPath) -> NSMutableAttributedString {
+        let rankGap = dailyBoxOffice.boxOfficeResult.dailyBoxOfficeList[indexPath.row].rankGap
+        let rankOldAndNew = dailyBoxOffice.boxOfficeResult.dailyBoxOfficeList[indexPath.row].rankOldAndNew
+        guard let intRankGap = Int(rankGap) else { return NSMutableAttributedString().makeColorToText(string: "", color: .red) }
+        
+        if rankOldAndNew == "NEW" {
+            return NSMutableAttributedString().makeColorToText(string: "신작", color: .red)
+        }
+        
+        switch intRankGap {
+        case -20 ... -1:
+            return NSMutableAttributedString().makeColorToText(string: "▼", color: .blue).makeColorToText(string: rankGap.trimmingCharacters(in: ["-"]), color: .black)
+        case 1...20:
+            return NSMutableAttributedString().makeColorToText(string: "▲", color: .red).makeColorToText(string: rankGap, color: .black)
+        case 0:
+            return NSMutableAttributedString().makeColorToText(string: "-", color: .black)
+        default:
+            return NSMutableAttributedString().makeColorToText(string: "", color: .black)
+        }
     }
 }
