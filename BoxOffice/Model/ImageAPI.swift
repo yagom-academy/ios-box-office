2e58ac3677c79493b9d2a9974a2fe402ba7da237
sehong <tjsgh0801@naver.com>
2023-03-31 14:45:50 +0900

    feat: 이미지검색 구현을 위한 ImageAPI 모델 구현

---
+++ BoxOffice/Model/ImageAPI.swift
@@ -0,0 +1,50 @@
+//
+//  ImageAPI.swift
+//  BoxOffice
+//
+//  Created by Seoyeon Hong on 2023/03/31.
+//
+
+import Foundation
+
+enum ImageAPI {
+    case imageSearchQuery(query: String)
+    
+    static func imageQuery(_ movieName: String) -> String {
+        return "\(movieName) 영화 포스터"
+    }
+}
+
+extension ImageAPI {
+    
+    var key: String {
+        return "add584f521686e2b3defebd44c63cdfb"
+    }
+    
+    var baseURL: String {
+        return "https://dapi.kakao.com"
+    }
+    
+    var path: String {
+        switch self {
+        case .imageSearchQuery:
+            return "/v2/search/image"
+        }
+    }
+    
+    var method: String {
+        return HTTPMethod.get
+    }
+    
+    var parameter: [String: String] {
+        switch self {
+        case .imageSearchQuery(let query):
+            return ["query": query, "page": "1", "size": "1"]
+        }
+    }
+    
+    var headers: [String: String] {
+        return ["Authorization": "KakaoAK \(key)"]
+    }
+    
+}
