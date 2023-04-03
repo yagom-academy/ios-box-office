73bca35991b261eababa1883a0f4195ba3753bff
ROWAN <kimkj93@naver.com>
2023-04-03 14:21:25 +0900

    refactor: merge 후 코드 및 파일 정리

--- BoxOffice/Utility/Extension/extension+String.swift
+++ BoxOffice/Utility/Extension/extension+String.swift
@@ -7,15 +7,8 @@
 
 import Foundation
 
-<<<<<<<< HEAD:BoxOffice/Utility/NumberFormat.swift
-extension NumberFormatter {
-    static let shared = NumberFormatter()
-    
-    func decimal(target: String) -> String? {
-========
 extension String {
     func decimal() -> String? {
->>>>>>>> step03-CustomCell:BoxOffice/Utility/Extension/extension+String.swift
         let numberFormatter = NumberFormatter()
         numberFormatter.numberStyle = .decimal
         let result = numberFormatter.string(from: Int(self) as? NSNumber ?? 0)
