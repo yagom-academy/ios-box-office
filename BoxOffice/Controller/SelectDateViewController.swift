64a4a18394addbef0b07c61b2bbc35122bbc0fe3
kkuldak1@naver.com <kkuldak1@naver.com>
2023-04-03 18:43:06 +0900

    feat: SelectDateViewController에서 현재 선택된 날짜 표시하는 기능 구현, 오늘 날짜 선택할 경우 Alert 표시 기능 구현

--- BoxOffice/Controller/SelectDateViewController.swift
+++ BoxOffice/Controller/SelectDateViewController.swift
@@ -14,6 +14,7 @@ final class SelectDateViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .white
         view.addSubview(calendarView)
         
@@ -21,13 +22,16 @@ final class SelectDateViewController: UIViewController {
     }
     
     private func configureCalendarView() {
-        let gregorianCalendar = Calendar(identifier: .gregorian)
-        calendarView.calendar = gregorianCalendar
+        calendarView.calendar = Calendar(identifier: .gregorian)
         calendarView.locale = Locale(identifier: "ko-KR")
         calendarView.fontDesign = .rounded
         calendarView.availableDateRange = DateInterval(start: .distantPast, end: .now)
         
+        guard let selectedDate = delegate?.selectedDate else { return }
+        
         let dateSelection = UICalendarSelectionSingleDate(delegate: self)
+        let dataComponent = calendarView.calendar.dateComponents([.year, .month, .day], from: selectedDate)
+        dateSelection.setSelected(dataComponent, animated: true)
         calendarView.selectionBehavior = dateSelection
         
         calendarView.translatesAutoresizingMaskIntoConstraints = false
@@ -41,15 +45,25 @@ final class SelectDateViewController: UIViewController {
 }
 
 @available(iOS 16.0, *)
-extension SelectDateViewController: UICalendarViewDelegate, UICalendarSelectionSingleDateDelegate {
+extension SelectDateViewController: UICalendarSelectionSingleDateDelegate {
     func dateSelection(_ selection: UICalendarSelectionSingleDate, didSelectDate dateComponents: DateComponents?) {
         guard let selectedDate = dateComponents?.date else { return }
-    
+        
+        if calendarView.calendar.dateComponents([.year, .month, .day], from: selectedDate) ==
+            calendarView.calendar.dateComponents([.year, .month, .day], from: calendarView.availableDateRange.end) {
+            let message = "오늘 날짜는 집계중입니다.\n다시 선택해주세요."
+            let alert = UIAlertController(title: "알림", message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "닫기", style: .default)
+            alert.addAction(okAction)
+            
+            present(alert, animated: true)
+            
+            return
+        }
+        
         delegate?.selectedDate = selectedDate
         delegate?.refreshData()
         
         self.dismiss(animated: true)
     }
 }
-
-
