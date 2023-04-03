1499561ce7fbab4d8cad41bb2841462b24a1e51c
sehong <tjsgh0801@naver.com>
2023-04-04 00:45:18 +0900

    feat: 하드코딩으로 사용했던 DateFormatOption 2가지 만들어주기

---
+++ BoxOffice/Model/DateFormatOption.swift
@@ -0,0 +1,13 @@
+//
+//  DateFormatOption.swift
+//  BoxOffice
+//
+//  Created by Seoyeon Hong on 2023/04/04.
+//
+
+import Foundation
+
+enum DateFormatOption: String {
+    case title = "yyyy-MM-dd"
+    case api = "yyyyMMdd"
+}
