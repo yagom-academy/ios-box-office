fe474db80aab9f87c0dec6a493466b7a0a427dc8
minsu3007 <minsu3007@gmail.com>
2023-04-03 14:11:46 +0900

    docs: MovieDetailViewController 생성

---
+++ Controller/MovieDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MovieDetailViewController.swift
+//  BoxOffice
+//
+//  Created by Andrew, 레옹아범 on 2023/04/03.
+//
+
+import UIKit
+
+final class MovieDetailViewController: UIViewController {
+    
+    private let scrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return scrollView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
