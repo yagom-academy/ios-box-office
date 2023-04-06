9d57f533adb84dfbf7b90e70f4beac43fa327751
parkmuri <smxhmu@naver.com>
2023-04-06 16:43:59 +0900

    refactor: 불필요한 코드 삭제

--- BoxOffice/Controller/DailyBoxOfficeViewController.swift
+++ BoxOffice/Controller/DailyBoxOfficeViewController.swift
@@ -28,7 +28,6 @@ final class DailyBoxOfficeViewController: UIViewController {
         configureDataSource()
         loadDailyBoxOffice(date: yesterday)
         configureRefreshControl()
-        loadDailyBoxOffice()
     }
     
     private func configureRootView() {
