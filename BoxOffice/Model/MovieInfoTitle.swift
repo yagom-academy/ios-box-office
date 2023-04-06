8ade377925f1ed074a741e4da393b35d9b3b282a
sehong <tjsgh0801@naver.com>
2023-04-06 19:52:10 +0900

    refactor: movieInfoTitle 분리

---
+++ BoxOffice/Model/MovieInfoTitle.swift
@@ -0,0 +1,19 @@
+//
+//  MovieInfoTitle.swift
+//  BoxOffice
+//
+//  Created by Seoyeon Hong on 2023/04/05.
+//
+
+import Foundation
+
+enum MovieInfoTitle {
+    static let director = "감독"
+    static let productionYear = "제작년도"
+    static let openDate = "개봉일"
+    static let showTime = "상영시간"
+    static let watchGradeName = "관람등급"
+    static let nationName = "제작국가"
+    static let genreName = "장르"
+    static let actorName = "배우"
+}
--- BoxOffice/View/DetailMovieInfoViewController.swift
+++ BoxOffice/View/DetailMovieInfoViewController.swift
@@ -9,17 +9,6 @@ import UIKit
 
 final class DetailMovieInfoViewController: UIViewController {
     
-    enum MovieInfoTitle {
-        static let director = "감독"
-        static let productionYear = "제작년도"
-        static let openDate = "개봉일"
-        static let showTime = "상영시간"
-        static let watchGradeName = "관람등급"
-        static let nationName = "제작국가"
-        static let genreName = "장르"
-        static let actorName = "배우"
-    }
-    
     private let movieCode: String
     private let movieName: String
     private let provider = APIProvider.shared
