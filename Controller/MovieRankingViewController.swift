7992498f6c796014a0873f46b8b161bc4c232b9a
minsu3007 <minsu3007@gmail.com>
2023-04-10 14:27:46 +0900

    refactor: configureNavigationTitle, makeMovieDescManager -> createMovieDescManager로 변경

--- Controller/MovieRankingViewController.swift
+++ Controller/MovieRankingViewController.swift
@@ -111,11 +111,7 @@ extension MovieRankingViewController: UICollectionViewDelegate {
 extension MovieRankingViewController {
     
     private func configureNavigationTitle() {
-        guard let navigationTitleText = dataManager?.navigationTitleText else {
-            return
-        }
-        
-        navigationItem.title = navigationTitleText
+        navigationItem.title = dataManager?.navigationTitleText
     }
     
     private func configureNavigationItems() {
