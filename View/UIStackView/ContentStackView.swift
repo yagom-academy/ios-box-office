f05e9871f6611b2228a63eddf7149e477cf8b4f5
Andrew-0411 <whk1140@naver.com>
2023-04-11 16:34:45 +0900

    refactor: Dynamic Type에 대응할 수 있도록 구현

--- View/UIStackView/ContentStackView.swift
+++ View/UIStackView/ContentStackView.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 final class ContentStackView: UIStackView {
+    
+    private var labelWidthLayout: NSLayoutConstraint?
 
     private let categoryLabel = {
         let font = UIFont.systemFont(ofSize: 12, weight: .bold)
         let fontMatrics = UIFontMetrics(forTextStyle: .headline).scaledFont(for: font)
         
         let label = UILabel(fontStyle: fontMatrics)
+        label.textAlignment = .center
         
         return label
     }()
@@ -22,7 +25,7 @@ final class ContentStackView: UIStackView {
         let font = UIFont.systemFont(ofSize: 12)
         let fontMatrics = UIFontMetrics(forTextStyle: .headline).scaledFont(for: font)
         
-        let label = UILabel(fontStyle: fontMatrics)
+        let label = UILabel(fontStyle: fontMatrics, numberOfLine: 0)
         
         return label
     }()
@@ -31,7 +34,7 @@ final class ContentStackView: UIStackView {
         self.init(frame: CGRect.zero)
         configure()
         
-        self.categoryLabel.text = categoryText
+        categoryLabel.text = categoryText
     }
     
     override init(frame: CGRect) {
@@ -47,12 +50,45 @@ final class ContentStackView: UIStackView {
     }
     
     private func configure() {
-        self.translatesAutoresizingMaskIntoConstraints = false
-        self.addArrangedSubview(categoryLabel)
-        self.addArrangedSubview(contentLabel)
-        self.spacing = 5
+        translatesAutoresizingMaskIntoConstraints = false
+        addArrangedSubview(categoryLabel)
+        addArrangedSubview(contentLabel)
+        spacing = 5
         
-        categoryLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.15).isActive = true
+        let mutiplierValue = makeWidthMultiplier()
+        
+        labelWidthLayout = categoryLabel.widthAnchor.constraint(equalTo: widthAnchor, multiplier: mutiplierValue)
+        labelWidthLayout?.isActive = true
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(changeWidthMultiplier), name: UIContentSizeCategory.didChangeNotification, object: nil)
     }
-
+    
+    private func changeWidth(by multiplier: CGFloat) {
+        guard let previousWidthLayout = labelWidthLayout else { return }
+        
+        NSLayoutConstraint.deactivate([
+            previousWidthLayout
+        ])
+        
+        labelWidthLayout = categoryLabel.widthAnchor.constraint(equalTo: widthAnchor, multiplier: multiplier)
+        labelWidthLayout?.isActive = true
+    }
+    
+    private func makeWidthMultiplier() -> CGFloat {
+        switch traitCollection.preferredContentSizeCategory {
+        case .accessibilityExtraExtraExtraLarge:
+            return 0.3
+        case .accessibilityExtraExtraLarge,
+             .accessibilityExtraLarge,
+             .accessibilityLarge:
+            return 0.25
+        default:
+            return 0.15
+        }
+    }
+    
+    @objc private func changeWidthMultiplier() {
+        let mutiplierValue = makeWidthMultiplier()
+        changeWidth(by: mutiplierValue)
+    }
 }
