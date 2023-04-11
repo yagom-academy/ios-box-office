seunghyunCheon/box-office

refactor: 비동기 함수 실행시 받아오는 데이터 네이밍 수정

BoxOffice/ViewController/BoxOfficeViewController.swift
@@ -77,8 +77,8 @@ final class BoxOfficeViewController: UIViewController {
         boxOfficeProvider.fetchData(.dailyBoxOffice(date: formattedSelectedDate),
                                     type: BoxOfficeDTO.self) { [weak self] result in
             switch result {
-            case .success(let data):
-                self?.boxOfficeItems = data.toDomain()
+            case .success(let boxOfficeDTOData):
+                self?.boxOfficeItems = boxOfficeDTOData.toDomain()
                 DispatchQueue.main.async {
                     self?.activityIndicator.stopAnimating()
                     self?.updateSnapshot()
@@ -115,8 +115,8 @@ final class BoxOfficeViewController: UIViewController {
         boxOfficeProvider.fetchData(.dailyBoxOffice(date: date),
                                     type: BoxOfficeDTO.self) { [weak self] result in
             switch result {
-            case .success(let data):
-                self?.boxOfficeItems = data.toDomain()
+            case .success(let boxOfficeDTOData):
+                self?.boxOfficeItems = boxOfficeDTOData.toDomain()
                 DispatchQueue.main.async {
                     self?.updateSnapshot()
                     self?.updateNavigationTitle(form: "yyyy-MM-dd", date: self?.yesterday)

BoxOffice/ViewController/MovieDetailViewController.swift
@@ -110,9 +110,9 @@ final class MovieDetailViewController: UIViewController {
         boxOfficeProvider.fetchData(.detailMovieInformation(movieCode: self.movieCode),
                                     type: MovieInformationDTO.self) { [weak self] result in
             switch result {
-            case .success(let data):
+            case .success(let movieInformationDTOData):
                 DispatchQueue.main.async {
-                    self?.movieDetailInformation = data.toDomain()
+                    self?.movieDetailInformation = movieInformationDTOData.toDomain()
                 }
             case .failure:
                 let alertViewController = AlertManager.shared.showFailureAlert()

yagom-academy/ios-box-office

BoxOffice/ViewController/BoxOfficeViewController.swift
@@ -77,8 +77,8 @@ final class BoxOfficeViewController: UIViewController {
         boxOfficeProvider.fetchData(.dailyBoxOffice(date: formattedSelectedDate),
                                     type: BoxOfficeDTO.self) { [weak self] result in
             switch result {
-            case .success(let data):
-                self?.boxOfficeItems = data.convertToBoxOfficeItems()
+            case .success(let boxOfficeDTOData):
+                self?.boxOfficeItems = boxOfficeDTOData.convertToBoxOfficeItems()
                 DispatchQueue.main.async {
                     self?.activityIndicator.stopAnimating()
                     self?.updateSnapshot()
@@ -115,8 +115,8 @@ final class BoxOfficeViewController: UIViewController {
         boxOfficeProvider.fetchData(.dailyBoxOffice(date: formattedYesterdayDate),
                                     type: BoxOfficeDTO.self) { [weak self] result in
             switch result {
-            case .success(let data):
-                self?.boxOfficeItems = data.convertToBoxOfficeItems()
+            case .success(let boxOfficeDTOData):
+                self?.boxOfficeItems = boxOfficeDTOData.convertToBoxOfficeItems()
                 DispatchQueue.main.async {
                     self?.updateSnapshot()
                     self?.updateNavigationTitle(form: "yyyy-MM-dd", date: self?.yesterday)

BoxOffice/ViewController/MovieDetailViewController.swift
@@ -110,9 +110,9 @@ final class MovieDetailViewController: UIViewController {
         boxOfficeProvider.fetchData(.detailMovieInformation(movieCode: self.movieCode),
                                     type: MovieInformationDTO.self) { [weak self] result in
             switch result {
-            case .success(let data):
+            case .success(let movieInformationDTOData):
                 DispatchQueue.main.async {
-                    self?.movieDetailInformation = data.convertToMovieDetailInformationItem()
+                    self?.movieDetailInformation = movieInformationDTOData.convertToMovieDetailInformationItem()
                 }
             case .failure:
                 let alertViewController = AlertManager.shared.showFailureAlert()