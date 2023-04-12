4358f52739b4c7737836adf89b2d3e6aea1cdc5e
minsu3007 <minsu3007@gmail.com>
2023-04-12 16:35:42 +0900

    refactor: 코드컨벤션 수정

--- BoxOffice/Model/RankingViewType.swift
+++ BoxOffice/Model/RankingViewType.swift
@@ -27,5 +27,4 @@ enum RankingViewType {
             self = .list
         }
     }
-    
 }
--- View/MovieRankingIconCell.swift
+++ View/MovieRankingIconCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MovieRankingIconCell: UICollectionViewCell {
+final class MovieRankingIconCell: UICollectionViewCell {
     static let identifier = "MovieRankingIconCell"
     
     // MARK: UI Properties
