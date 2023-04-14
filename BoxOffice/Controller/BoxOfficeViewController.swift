f4ef73b93ce39168b74b7740962f11f9d4dd99d8
kimseongj <ksj8277@naver.com>
2023-04-14 15:08:20 +0900

    feat: URLCache에 대한 용량 및 저장 경로 설정

--- BoxOffice/Controller/BoxOfficeViewController.swift
+++ BoxOffice/Controller/BoxOfficeViewController.swift
@@ -29,11 +29,16 @@ final class BoxOfficeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setURLCache()
         imageSearchService.removeCacheAfter30min()
         fetchDailyBoxOffice()
         setUpView()
         setCalendarViewDelegate()
         
+    }
+    
+    private func setURLCache() {
+        URLCache.shared = .init(memoryCapacity: 300 * 1024 * 1024, diskCapacity: 300 * 1024 * 1024, directory: FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0])
     }
     
     private func fetchDailyBoxOffice() {
