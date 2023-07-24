78d021ab32f37ac188aa386c1ee5742e812b2091
Zion <lion2212@naver.com>
2023-07-24 19:58:32 +0900

    feat: Decoding Test시 Error Print 추가

--- BoxOfficeTests/BoxOfficeTests.swift
+++ BoxOfficeTests/BoxOfficeTests.swift
@@ -16,14 +16,19 @@ final class BoxOfficeTests: XCTestCase {
         let decoder = JSONDecoder()
         
         guard let asset = NSDataAsset(name: "box_office_sample") else {
+            print("asset 변환 실패")
             return
         }
         
-        guard let sut = try? decoder.decode(BoxOfficeResult.self, from: asset.data) else {
-            return
+        do {
+            let sut = try decoder.decode(BoxOfficeResult.self, from: asset.data)
+            
+            self.sut = sut.daily
+        } catch {
+            guard let error = error as? DecodingError else { return }
+            
+            print(error)
         }
-        
-        self.sut = sut.daily
     }
     
     override func tearDownWithError() throws {
@@ -32,9 +37,44 @@ final class BoxOfficeTests: XCTestCase {
     
     func test_디코딩결과값_확인() {
         //given
-        let firstExpectation = MovieInformation(number: "1", rank: "1", rankInten: "0", rankOldAndNew: "NEW", movieCode: "20199882", movieName: "경관의 피", openDate: "2022-01-05", salesAmount: "584559330", salesShare: "34.2", salesInten: "584559330", salesChange: "100", salesAccumulate: "631402330", audienceCount: "64050", audienceInten: "64050", audienceChange: "100", audienceAccumulate: "69228", screenCount: "1171", showCount: "4416")
+        let firstExpectation = MovieInformation(number: "1",
+                                                rank: "1",
+                                                rankInten: "0",
+                                                rankOldAndNew: "NEW",
+                                                movieCode: "20199882",
+                                                movieName: "경관의 피",
+                                                openDate: "2022-01-05",
+                                                salesAmount: "584559330",
+                                                salesShare: "34.2",
+                                                salesInten: "584559330",
+                                                salesChange: "100",
+                                                salesAccumulate: "631402330",
+                                                audienceCount: "64050",
+                                                audienceInten: "64050",
+                                                audienceChange: "100",
+                                                audienceAccumulate: "69228",
+                                                screenCount: "1171",
+                                                showCount: "4416")
         
-        let lastExpectation = MovieInformation(number: "10", rank: "10", rankInten: "-3", rankOldAndNew: "OLD", movieCode: "20210864", movieName: "엔칸토: 마법의 세계", openDate: "2021-11-24", salesAmount: "2290000", salesShare: "0.1", salesInten: "-7180050", salesChange: "-75.8", salesAccumulate: "5964273080", audienceCount: "291", audienceInten: "-765", audienceChange: "-72.4", audienceAccumulate: "628878", screenCount: "23", showCount: "24")
+        let lastExpectation = MovieInformation(number: "10",
+                                               rank: "10",
+                                               rankInten: "-3",
+                                               rankOldAndNew: "OLD",
+                                               movieCode: "20210864",
+                                               movieName: "엔칸토: 마법의 세계",
+                                               openDate: "2021-11-24",
+                                               salesAmount: "2290000",
+                                               salesShare: "0.1",
+                                               salesInten: "-7180050",
+                                               salesChange: "-75.8",
+                                               salesAccumulate: "5964273080",
+                                               audienceCount: "291",
+                                               audienceInten: "-765",
+                                               audienceChange: "-72.4",
+                                               audienceAccumulate: "628878",
+                                               screenCount: "23",
+                                               showCount: "24")
+        
         //when
         let first = sut.dailyBoxOfficeList.first
         let last = sut.dailyBoxOfficeList.last
@@ -43,7 +83,4 @@ final class BoxOfficeTests: XCTestCase {
         XCTAssertEqual(firstExpectation, first)
         XCTAssertEqual(lastExpectation, last)
     }
-    
-    
-    
 }
