0deda82c8ca188f94abf6c4e731073030d4074b6
idinaloq <idinaloq@icloud.com>
2023-07-24 21:54:47 +0900

    ✅Test: BoxOfficeTest추가
    refactor: 타입 구조 변경

--- BoxOffice/Model/DailyBoxOffice.swift
+++ BoxOffice/Model/DailyBoxOffice.swift
@@ -9,46 +9,12 @@ struct DailyBoxOffice: Decodable {
     let boxOfficeType: String
     let showRange: String
     let dailyBoxOfficeList: [MovieInformation]
-}
-
-struct MovieInformation: Decodable {
-    let rowNumber: String
-    let rank: String
-    let rankChangeValue: String
-    let rankOldAndNew: String
-    let movieCode: String
-    let movieName: String
-    let openDate: String
-    let salesAmount: String
-    let salesShare: String
-    let salesChangeValue: String
-    let salesChangeRatio: String
-    let salesAccumulate: String
-    let audienceCount: String
-    let audienceChangeValue: String
-    let audienceChangeRatio: String
-    let audienceAccumulate: String
-    let screenCount: String
-    let showCount: String
     
     enum CodingKeys: String, CodingKey {
-        case rowNumber = "rnum"
-        case rank
-        case rankChangeValue = "rankInten"
-        case rankOldAndNew
-        case movieCode = "movieCd"
-        case movieName = "movieNm"
-        case openDate = "openDt"
-        case salesAmount = "salesAmt"
-        case salesShare
-        case salesChangeValue = "salesInten"
-        case salesChangeRatio = "salesChange"
-        case salesAccumulate = "salesAcc"
-        case audienceCount = "audiCnt"
-        case audienceChangeValue = "audiInten"
-        case audienceChangeRatio = "audiChange"
-        case audienceAccumulate = "audiAcc"
-        case screenCount = "scrnCnt"
-        case showCount = "showCnt"
+        case boxOfficeType = "boxofficeType"
+        case showRange
+        case dailyBoxOfficeList
     }
 }
+
+
---
+++ BoxOffice/Model/MovieInformation.swift
@@ -0,0 +1,48 @@
+//
+//  MovieInformation.swift
+//  BoxOffice
+//
+//  Created by Idinaloq, MARY on 2023/07/24.
+//
+
+struct MovieInformation: Decodable {
+    let rowNumber: String
+    let rank: String
+    let rankChangeValue: String
+    let rankOldAndNew: String
+    let movieCode: String
+    let movieName: String
+    let openDate: String
+    let salesAmount: String
+    let salesShare: String
+    let salesChangeValue: String
+    let salesChangeRatio: String
+    let salesAccumulate: String
+    let audienceCount: String
+    let audienceChangeValue: String
+    let audienceChangeRatio: String
+    let audienceAccumulate: String
+    let screenCount: String
+    let showCount: String
+    
+    enum CodingKeys: String, CodingKey, CaseIterable {
+        case rowNumber = "rnum"
+        case rank
+        case rankChangeValue = "rankInten"
+        case rankOldAndNew
+        case movieCode = "movieCd"
+        case movieName = "movieNm"
+        case openDate = "openDt"
+        case salesAmount = "salesAmt"
+        case salesShare
+        case salesChangeValue = "salesInten"
+        case salesChangeRatio = "salesChange"
+        case salesAccumulate = "salesAcc"
+        case audienceCount = "audiCnt"
+        case audienceChangeValue = "audiInten"
+        case audienceChangeRatio = "audiChange"
+        case audienceAccumulate = "audiAcc"
+        case screenCount = "scrnCnt"
+        case showCount = "showCnt"
+    }
+}
