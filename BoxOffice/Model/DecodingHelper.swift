b03a99a5a1e52c6f989dc7a7912dff37a1448b35
JusBug <pksgwo123@gmail.com>
2023-07-24 19:07:33 +0900

    feat: DecoingHelper에 json파일을 파싱하는 decode() 구현

--- BoxOffice/Model/DecodingHelper.swift
+++ BoxOffice/Model/DecodingHelper.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct DecodingHelper {
+    
+    func decode() -> MovieInfo? {
+        let decoder = JSONDecoder()
+        
+        guard let path = Bundle.main.path(forResource: "box_office_sample", ofType: "json") else {
+            print("해당 파일을 찾을 수 없습니다")
+            return nil
+        }
+        
+        guard let data = try? String(contentsOfFile: path).data(using: .utf8) else {
+            print("해당 파일을 읽어 올 수 없습니다.")
+            return nil
+        }
+        
+        guard let result = try? decoder.decode(MovieInfo.self, from: data) else {
+            print("해당 파일을 변환하는데 실패했습니다.")
+            return nil
+        }
+        
+        return result
+    }
+}
