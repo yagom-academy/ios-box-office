37f4f6586649ccaf806b88fab4d3535cc54a6369
mint3382 <mskim0926@naver.com>
2023-07-25 00:19:09 +0900

    feat: BoxOfficeResult 타입 구현, BoxOfficeTests 파일 생성

---
+++ BoxOffice/Model/BoxOfficeResult.swift
@@ -0,0 +1,22 @@
+//
+//  BoxOfficeResult.swift
+//  BoxOffice
+//
+//  Created by hoon, mint on 2023/07/24.
+//
+
+struct BoxOfficeResult: Decodable {
+    let boxOfficeType: String
+    let showRange: String
+    let dailyBoxOfficeList: [BoxOfficeItemInformation]
+    
+    enum CodingKeys: String, CodingKey {
+        case boxOfficeType = "boxofficeType"
+        case showRange
+        case dailyBoxOfficeList
+    }
+}
+
+struct BoxOfficeItemInformation: Decodable {
+    
+}
---
+++ BoxOfficeTests/JsonDecodeTests.swift
@@ -0,0 +1,20 @@
+//
+//  JsonDecodeTests.swift
+//  JsonDecodeTests
+//
+//  Created by hoon, mint on 2023/07/24.
+//
+
+import XCTest
+
+final class JsonDecodeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
