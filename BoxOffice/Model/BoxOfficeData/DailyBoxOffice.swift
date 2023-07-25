07a5c1908f7c9daec432d75d1ea7070c26b5952d
mint3382 <mskim0926@naver.com>
2023-07-25 15:03:43 +0900

    test: JSONDecodeTests 구현

---
+++ BoxOffice/Model/BoxOfficeData/DailyBoxOffice.swift
@@ -0,0 +1,10 @@
+//
+//  DailyBoxOffice.swift
+//  BoxOffice
+//
+//  Created by hoon, mint on 2023/07/25.
+//
+
+struct DailyBoxOffice: Decodable {
+    let boxOfficeResult: BoxOfficeResult
+}
--- BoxOfficeTests/JsonDecodeTests.swift
+++ BoxOfficeTests/JsonDecodeTests.swift
@@ -6,15 +6,85 @@
 //
 
 import XCTest
+@testable import BoxOffice
+
+extension BoxOfficeItemInformation: Equatable {
+    public static func == (lhs: BoxOfficeItemInformation, rhs: BoxOfficeItemInformation) -> Bool {
+        return lhs.movieCode == rhs.movieCode
+    }
+}
 
 final class JsonDecodeTests: XCTestCase {
+    var sut: DailyBoxOffice!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        let bundle = Bundle(for: JsonDecodeTests.self)
+        guard let json = NSDataAsset(name: "box_office_sample", bundle: bundle) else {
+            return
+        }
+        sut = try JSONDecoder().decode(DailyBoxOffice.self, from: json.data)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+        sut = nil
+    }
+    
+    func test_boxOfficeSample_디코딩하면_일별박스오피스를_반환한다() {
+        //given
+        let result = sut.boxOfficeResult.boxOfficeType
+        
+        //when
+        let expectation = "일별 박스오피스"
+        
+        //then
+        XCTAssertEqual(result, expectation)
     }
 
+    func test_boxOfficeSample_디코딩하면_1번_아이템의_rankOldAndNew에서_new를_반환한다() {
+        //given
+        let result = sut.boxOfficeResult.dailyBoxOfficeList.first?.rankOldAndNew
+        
+        //when
+        let expectation = BoxOfficeItemInformation.RankOldAndNew.new
+        
+        //then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_boxOfficeSample_디코딩하면_daliyBoxOfficeList에서_BoxOfficeItemInformation을_반환한다() throws {
+        //given
+        let result = sut.boxOfficeResult.dailyBoxOfficeList.first
+        let json =
+            """
+            {"rnum":"1",
+            "rank":"1",
+            "rankInten":"0",
+            "rankOldAndNew":"NEW",
+            "movieCd":"20199882",
+            "movieNm":"경관의 피",
+            "openDt":"2022-01-05",
+            "salesAmt":"584559330",
+            "salesShare":"34.2",
+            "salesInten":"584559330",
+            "salesChange":"100",
+            "salesAcc":"631402330",
+            "audiCnt":"64050",
+            "audiInten":"64050",
+            "audiChange":"100",
+            "audiAcc":"69228",
+            "scrnCnt":"1171",
+            "showCnt":"4416"}
+            """
+        
+        //when
+        guard let jsonData = json.data(using: .utf8) else {
+            return
+        }
+        let expectation = try JSONDecoder().decode(BoxOfficeItemInformation.self, from: jsonData)
+        
+        //then
+        XCTAssertEqual(result, expectation)
+    }
 }
