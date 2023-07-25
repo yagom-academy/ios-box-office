f63736e0b89a5a9529f9fb5deff5d9d032652c33
bubblecocoa <bubblecocoa@gmail.com>
2023-07-25 09:58:41 +0900

    test: BoxOfficeTests, DailyBoxOfficeTests 파일 생성

---
+++ BoxOfficeTests/BoxOfficeResult.swift
@@ -0,0 +1,24 @@
+//
+//  BoxOfficeTests.swift
+//  BoxOfficeTests
+//
+//  Created by Serana, BMO on 2023/07/25.
+//
+
+import XCTest
+@testable import BoxOffice
+
+final class BoxOfficeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+    
+    func test_() {
+        
+    }
+}
