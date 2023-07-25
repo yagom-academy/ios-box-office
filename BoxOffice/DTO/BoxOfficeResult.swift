77ce056f523bc5ab37d2b7728e5a186f2507e802
bubblecocoa <bubblecocoa@gmail.com>
2023-07-25 19:26:34 +0900

    comment: 필드에 필요한 설명 작성

--- BoxOffice/DTO/BoxOfficeResult.swift
+++ BoxOffice/DTO/BoxOfficeResult.swift
@@ -6,9 +6,9 @@
 //
 
 struct BoxOfficeResult: Decodable {
-    let boxOfficeType: String
-    let showRange: String
-    let dailyBoxOfficeList: [DailyBoxOffice]
+    let boxOfficeType: String                   // 박스오피스 종류
+    let showRange: String                       // 조회 일자 범위
+    let dailyBoxOfficeList: [DailyBoxOffice]    // 일별 박스오피스 목록
     
     private enum CodingKeys: String, CodingKey {
         case boxOfficeType = "boxofficeType"
