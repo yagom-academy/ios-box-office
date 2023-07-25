c3a791abce11638cdb3c14b26050bac9a0b35aab
yyss99 <yyssjj9999@gmail.com>
2023-07-25 11:36:25 +0900

    ♻️refactor: BoxOfficeModel에 코딩키 추가

--- BoxOffice/Model/BoxOfficeModel.swift
+++ BoxOffice/Model/BoxOfficeModel.swift
@@ -5,33 +5,60 @@
 //  Created by Kobe, yyss99 on 2023/07/25.
 //
 
-struct BoxOffice {
+struct BoxOffice: Decodable {
 	let boxOfficeResult: BoxOfficeResult
 }
 
 struct BoxOfficeResult: Decodable {
-	let boxofficeType: String
+	let boxOfficeType: String
 	let showRange: String
 	let dailyBoxOfficeList: [DailyBoxOfficeList]
+    
+    private enum CodingKeys: String, CodingKey {
+        case boxOfficeType = "boxofficeType"
+        case showRange
+        case dailyBoxOfficeList
+    }
 }
 
 struct DailyBoxOfficeList: Decodable {
-	let rnum: String
+	let rankNumber: String
 	let rank: String
-	let rankInten: String
+	let rankIntensity: String
 	let rankOldAndNew: String
-	let movieCd: String
-	let movieNm: String
-	let openDt: String
-	let salesAmt: String
+	let movieCode: String
+	let movieNumber: String
+	let openDate: String
+	let salesAmount: String
 	let salesShare: String
-	let salesInten: String
+	let salesIntensity: String
 	let salesChange: String
-	let salesAcc: String
-	let audiCnt: String
-	let audiInten: String
-	let audiChange: String
-	let audiAcc: String
-	let scrnCnt: String
-	let showCnt: String
+	let salesAccumulated: String
+	let audienceCount: String
+	let audienceIntensity: String
+	let audienceChange: String
+	let audienceAccumulated: String
+	let screenCount: String
+	let showCount: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case rankNumber = "rnum"
+        case rank
+        case rankIntensity = "rankInten"
+        case rankOldAndNew
+        case movieCode = "movieCd"
+        case movieNumber = "movieNm"
+        case openDate = "openDt"
+        case salesAmount = "salesAmt"
+        case salesShare
+        case salesIntensity = "salesInten"
+        case salesChange
+        case salesAccumulated = "salesAcc"
+        case audienceCount = "audiCnt"
+        case audienceIntensity = "audiInten"
+        case audienceChange = "audiChange"
+        case audienceAccumulated = "audiAcc"
+        case screenCount = "scrnCnt"
+        case showCount = "showCnt"
+    }
 }
