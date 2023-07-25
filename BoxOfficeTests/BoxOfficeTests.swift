7462dd6217542420af62504b6e2ca0c7715661b0
Serena <sha0720@naver.com>
2023-07-25 15:19:10 +0900

    test: BoxOfficeTests에 테스트케이스 작성
    test_box_office_sample을_Decode하면_BoxOfficeResult_인스턴스가_존재한다

--- BoxOfficeTests/BoxOfficeTests.swift
+++ BoxOfficeTests/BoxOfficeTests.swift
@@ -9,16 +9,23 @@ import XCTest
 @testable import BoxOffice
 
 final class BoxOfficeTests: XCTestCase {
-
+    var sut: BoxOffice!
     override func setUpWithError() throws {
         try super.setUpWithError()
+        sut = JSONDecoder.decode(fileName: "box_office_sample")
     }
 
     override func tearDownWithError() throws {
+        sut = nil
         try super.tearDownWithError()
     }
     
-    func test_() {
+    func test_box_office_sample을_Decode하면_BoxOfficeResult_인스턴스가_존재한다() {
+        // Given
         
+        // When
+        
+        // Then
+        XCTAssertNotNil(sut.boxOfficeResult)
     }
 }
