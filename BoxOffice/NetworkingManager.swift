6005fe89cb36ed9205bb528c323bf93c9e64c3b2
Min Hyun <minnimida@gmail.com>
2023-07-27 12:07:05 +0900

    refactor: NetworkingManager 데이터 전달 방식 변경
    - delegate 패턴에서 escaping 클로저 활용 방식으로 변경
    - ViewController에 setUpNetwork, loadData 메서드 생성

--- BoxOffice/NetworkingManager.swift
+++ BoxOffice/NetworkingManager.swift
@@ -7,29 +7,30 @@
 import Foundation
 
 struct NetworkingManager {
-    let session = URLSession.shared
-    var delegate: NetworkingDelegate?
+    let session: URLSession
     
-    func load(_ urlString: String) {
+    init(_ session: URLSession) {
+        self.session = session
+    }
+    
+    func load(_ urlString: String, completion: @escaping (Result<Data, BoxOfficeError>) -> Void) {
         guard let url = URL(string: urlString) else {
             return
         }
         
         let task = session.dataTask(with: url) { data, response, error in
-            // 연결 에러
             if error != nil {
-                print(BoxOfficeError.connectionFailure.description)
+                completion(.failure(BoxOfficeError.connectionFailure))
                 return
             }
             
-            // 서버 에러
             guard let httpResponse = response as? HTTPURLResponse else {
-                print(BoxOfficeError.notHttpUrlResponse.description)
+                completion(.failure(BoxOfficeError.notHttpUrlResponse))
                 return
             }
             
             guard (200...299).contains(httpResponse.statusCode) else {
-                print(BoxOfficeError.invalidResponse(statusCode: httpResponse.statusCode).description)
+                completion(.failure(BoxOfficeError.invalidResponse(statusCode: httpResponse.statusCode)))
                 return
             }
             
@@ -38,7 +39,7 @@ struct NetworkingManager {
                let rawData = data,
                let string = String(data: rawData, encoding: .utf8),
                let data = string.data(using: .utf8) {
-                delegate?.setReceivedData(data)
+                completion(.success(data))
             }
         }
         task.resume()
--- BoxOffice/ViewController.swift
+++ BoxOffice/ViewController.swift
@@ -7,29 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    var networkManager: NetworkingManager = NetworkingManager()
+class ViewController: UIViewController, URLSessionDelegate {
+    var networkingManager: NetworkingManager?
     let url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=%@&movieCd=20124079"
     
     var receivedData: MovieDetailEntity?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        networkManager.delegate = self
+        setUpNetwork()
+        loadData()
+    }
+    
+    private func setUpNetwork() {
+        let session = {
+            let configuration = URLSessionConfiguration.default
+            configuration.waitsForConnectivity = true
+            return URLSession(configuration: configuration, delegate: self, delegateQueue: nil)
+        }()
         
+        networkingManager = NetworkingManager(session)
+    }
+    
+    private func loadData() {
         if let keyPath = Bundle.main.infoDictionary?["API_KEY"] as? String {
-            networkManager.load(String(format: url, keyPath))
+            networkingManager?.load(String(format: url, keyPath)) { [weak self] (result: Result<Data, BoxOfficeError>) in
+                switch result {
+                case .success(let data):
+                    guard let decodedData: MovieDetailEntity = DecodingManager.shared.decode(data) else {
+                        return
+                    }
+                    self?.receivedData = decodedData
+                case .failure(let error):
+                    print(error.description)
+                }
+            }
         }
     }
 }
 
-
-extension ViewController: NetworkingDelegate {
-    func setReceivedData(_ data: Data) {
-        guard let newData: MovieDetailEntity = DecodingManager.shared.decode(data) else {
-            return
-        }
-        
-        receivedData = newData
-    }
-}
