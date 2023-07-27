5104c8b74670c1c4dbaa1337d661870d8353baf8
idinaloq <idinaloq@icloud.com>
2023-07-27 19:56:41 +0900

    ✨feat: MovieService 타입 생성

---
+++ BoxOffice/Model/MovieService.swift
@@ -0,0 +1,29 @@
+//
+//  MovieService.swift
+//  BoxOffice
+//
+//  Created by Idinaloq, MARY on 2023/07/27.
+//
+
+import Foundation
+
+struct MovieService {
+    func fetchData(url: URL) {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                        (200...299).contains(httpResponse.statusCode) else {
+                        return
+                    }
+            
+            if let data = data {
+                let decodedData = try? JSONDecoder().decode(BoxOffice.self, from: data)
+            }
+        }
+        
+        task.resume()
+    }
+}
