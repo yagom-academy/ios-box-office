075b16e94dbe292a5a2533663e8aa22152d67d06
iOS-Yetti <yechan123456@gmail.net>
2023-07-27 11:41:36 +0900

    refactor: BoxOfficeEntity, MovieDetailEntity 중첩타입 extension으로 분리

--- BoxOffice/BoxOfficeEntity.swift
+++ BoxOffice/BoxOfficeEntity.swift
@@ -6,42 +6,46 @@
 //
 
 struct BoxOfficeEntity: Decodable {
+    let boxOfficeResult: BoxOfficeResult
+}
+
+extension BoxOfficeEntity {
     struct BoxOfficeResult: Decodable {
-        struct DailyBoxOffice: Decodable {
-            let resultNumber, rank, rankIntensity, rankOldAndNew, movieCode,
-                movieName, openDate, salesAmount, salesShare, salesIntensity,
-                salesChange, salesAccumulate, audienceCount, audienceIntensity,
-                audienceChange, audienceAccumulate, screenCount, showCount: String
-            
-            enum CodingKeys: String, CodingKey {
-                case rank, rankOldAndNew, salesShare, salesChange
-                case resultNumber = "rnum"
-                case rankIntensity = "rankInten"
-                case movieCode = "movieCd"
-                case movieName = "movieNm"
-                case openDate = "openDt"
-                case salesAmount = "salesAmt"
-                case salesIntensity = "salesInten"
-                case salesAccumulate = "salesAcc"
-                case audienceCount = "audiCnt"
-                case audienceIntensity = "audiInten"
-                case audienceChange = "audiChange"
-                case audienceAccumulate = "audiAcc"
-                case screenCount = "scrnCnt"
-                case showCount = "showCnt"
-            }
-        }
+        let boxOfficeType, showRange: String
+        let dailyBoxOfficeList: [DailyBoxOffice]
         
         enum CodingKeys: String, CodingKey {
             case boxOfficeType = "boxofficeType"
             case showRange, dailyBoxOfficeList
         }
-        
-        let boxOfficeType, showRange: String
-        let dailyBoxOfficeList: [DailyBoxOffice]
     }
-    
-    let boxOfficeResult: BoxOfficeResult
+}
+
+extension BoxOfficeEntity.BoxOfficeResult {
+    struct DailyBoxOffice: Decodable {
+        let resultNumber, rank, rankIntensity, rankOldAndNew, movieCode,
+            movieName, openDate, salesAmount, salesShare, salesIntensity,
+            salesChange, salesAccumulate, audienceCount, audienceIntensity,
+            audienceChange, audienceAccumulate, screenCount, showCount: String
+        
+        enum CodingKeys: String, CodingKey {
+            case rank, rankOldAndNew, salesShare, salesChange
+            case resultNumber = "rnum"
+            case rankIntensity = "rankInten"
+            case movieCode = "movieCd"
+            case movieName = "movieNm"
+            case openDate = "openDt"
+            case salesAmount = "salesAmt"
+            case salesIntensity = "salesInten"
+            case salesAccumulate = "salesAcc"
+            case audienceCount = "audiCnt"
+            case audienceIntensity = "audiInten"
+            case audienceChange = "audiChange"
+            case audienceAccumulate = "audiAcc"
+            case screenCount = "scrnCnt"
+            case showCount = "showCnt"
+        }
+    }
 }
 
 
--- BoxOffice/MovieDetailEntity.swift
+++ BoxOffice/MovieDetailEntity.swift
@@ -6,127 +6,15 @@
 //
 
 struct MovieDetailEntity: Decodable {
+    let movieInformationResult: MovieInformationResult
+    
+    enum CodingKeys: String, CodingKey {
+        case movieInformationResult = "movieInfoResult"
+    }
+}
+
+extension MovieDetailEntity {
     struct MovieInformationResult: Decodable {
-        struct MovieInformation: Decodable {
-            struct Nation: Decodable {
-                let name: String
-                
-                enum CodingKeys: String, CodingKey {
-                    case name = "nationNm"
-                }
-            }
-            
-            struct Genre: Decodable {
-                let name: String
-                
-                enum CodingKeys: String, CodingKey {
-                    case name = "genreNm"
-                }
-            }
-            
-            struct Director: Decodable {
-                let name: String
-                let englishName: String
-                
-                enum CodingKeys: String, CodingKey {
-                    case name = "peopleNm"
-                    case englishName = "peopleNmEn"
-                }
-            }
-            
-            struct Actor: Decodable {
-                let name: String
-                let englishName: String
-                let castName: String
-                let englishCastName: String
-                
-                enum CodingKeys: String, CodingKey {
-                    case name = "peopleNm"
-                    case englishName = "peopleNmEn"
-                    case castName = "cast"
-                    case englishCastName = "castEn"
-                }
-            }
-            
-            struct ShowType: Decodable {
-                let groupName: String
-                let name: String
-                
-                enum CodingKeys: String, CodingKey {
-                    case groupName = "showTypeGroupNm"
-                    case name = "showTypeNm"
-                }
-            }
-            
-            struct Company: Decodable {
-                let code: String
-                let name: String
-                let englishName: String
-                let role: String
-                
-                enum CodingKeys: String, CodingKey {
-                    case code = "companyCd"
-                    case name = "companyNm"
-                    case englishName = "companyNmEn"
-                    case role = "companyPartNm"
-                }
-            }
-            
-            struct Audit: Decodable {
-                let auditNumber: String
-                let movieRating: String
-                
-                enum CodingKeys: String, CodingKey {
-                    case auditNumber = "auditNo"
-                    case movieRating = "watchGradeNm"
-                }
-            }
-            
-            struct Staff: Decodable {
-                let name: String
-                let englishName: String
-                let role: String
-                
-                enum CodingKeys: String, CodingKey {
-                    case name = "peopleNm"
-                    case englishName = "peopleNmEn"
-                    case role = "staffRoleNm"
-                }
-            }
-            
-            let movieCode: String
-            let movieName: String
-            let englishMovieName: String
-            let originalMovieName: String
-            let showTime: String
-            let productionYear: String
-            let openingDate: String
-            let productionStatusName: String
-            let typeName: String
-            let nations: [Nation]
-            let genres: [Genre]
-            let directors: [Director]
-            let actors: [Actor]
-            let showTypes: [ShowType]
-            let companies: [Company]
-            let audits: [Audit]
-            let staffs: [Staff]
-            
-            enum CodingKeys: String, CodingKey {
-                case nations, genres, directors, actors, showTypes, audits, staffs
-                
-                case movieCode = "movieCd"
-                case movieName = "movieNm"
-                case englishMovieName = "movieNmEn"
-                case originalMovieName = "movieNmOg"
-                case showTime = "showTm"
-                case productionYear = "prdtYear"
-                case openingDate = "openDt"
-                case productionStatusName = "prdtStatNm"
-                case typeName = "typeNm"
-                case companies = "companys"
-            }
-        }
         
         let movieInformation: MovieInformation
         let source: String
@@ -136,10 +24,129 @@ struct MovieDetailEntity: Decodable {
             case movieInformation = "movieInfo"
         }
     }
+}
+
+extension MovieDetailEntity.MovieInformationResult {
+    struct MovieInformation: Decodable {
+        let movieCode: String
+        let movieName: String
+        let englishMovieName: String
+        let originalMovieName: String
+        let showTime: String
+        let productionYear: String
+        let openingDate: String
+        let productionStatusName: String
+        let typeName: String
+        let nations: [Nation]
+        let genres: [Genre]
+        let directors: [Director]
+        let actors: [Actor]
+        let showTypes: [ShowType]
+        let companies: [Company]
+        let audits: [Audit]
+        let staffs: [Staff]
+        
+        enum CodingKeys: String, CodingKey {
+            case nations, genres, directors, actors, showTypes, audits, staffs
+            
+            case movieCode = "movieCd"
+            case movieName = "movieNm"
+            case englishMovieName = "movieNmEn"
+            case originalMovieName = "movieNmOg"
+            case showTime = "showTm"
+            case productionYear = "prdtYear"
+            case openingDate = "openDt"
+            case productionStatusName = "prdtStatNm"
+            case typeName = "typeNm"
+            case companies = "companys"
+        }
+    }
+}
+
+extension MovieDetailEntity.MovieInformationResult.MovieInformation {
+    struct Nation: Decodable {
+        let name: String
+        
+        enum CodingKeys: String, CodingKey {
+            case name = "nationNm"
+        }
+    }
+
+    struct Genre: Decodable {
+        let name: String
+        
+        enum CodingKeys: String, CodingKey {
+            case name = "genreNm"
+        }
+    }
     
-    let movieInformationResult: MovieInformationResult
+    struct Director: Decodable {
+        let name: String
+        let englishName: String
+        
+        enum CodingKeys: String, CodingKey {
+            case name = "peopleNm"
+            case englishName = "peopleNmEn"
+        }
+    }
     
-    enum CodingKeys: String, CodingKey {
-        case movieInformationResult = "movieInfoResult"
+    struct Actor: Decodable {
+        let name: String
+        let englishName: String
+        let castName: String
+        let englishCastName: String
+        
+        enum CodingKeys: String, CodingKey {
+            case name = "peopleNm"
+            case englishName = "peopleNmEn"
+            case castName = "cast"
+            case englishCastName = "castEn"
+        }
+    }
+    
+    struct ShowType: Decodable {
+        let groupName: String
+        let name: String
+        
+        enum CodingKeys: String, CodingKey {
+            case groupName = "showTypeGroupNm"
+            case name = "showTypeNm"
+        }
+    }
+    
+    struct Company: Decodable {
+        let code: String
+        let name: String
+        let englishName: String
+        let role: String
+        
+        enum CodingKeys: String, CodingKey {
+            case code = "companyCd"
+            case name = "companyNm"
+            case englishName = "companyNmEn"
+            case role = "companyPartNm"
+        }
+    }
+    
+    struct Audit: Decodable {
+        let auditNumber: String
+        let movieRating: String
+        
+        enum CodingKeys: String, CodingKey {
+            case auditNumber = "auditNo"
+            case movieRating = "watchGradeNm"
+        }
+    }
+    
+    struct Staff: Decodable {
+        let name: String
+        let englishName: String
+        let role: String
+        
+        enum CodingKeys: String, CodingKey {
+            case name = "peopleNm"
+            case englishName = "peopleNmEn"
+            case role = "staffRoleNm"
+        }
     }
 }
