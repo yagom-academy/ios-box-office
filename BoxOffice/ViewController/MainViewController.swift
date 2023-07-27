fd14afac76b2703134ebd23b29ab5a940a390e39
hemg2 <hemg2@naver.com>
2023-07-27 19:15:48 +0900

    feat: MainViewController-> RootViewController 지정

---
+++ BoxOffice/ViewController/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  BoxOffice
+//
+//  Created by Zion, Hemg on 13/01/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
