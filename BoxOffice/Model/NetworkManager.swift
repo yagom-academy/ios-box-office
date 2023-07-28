a59328f52df2905fa51a5d5c9d25d64f8aa36590
Kobe <mskang@xingxingmobility.com>
2023-07-28 16:11:27 +0900

    :recycle: Refactor: getBoxOfficeData 메서드 생성 및 구현.

--- BoxOffice/Model/NetworkManager.swift
+++ BoxOffice/Model/NetworkManager.swift
@@ -19,4 +19,30 @@ struct NetworkManager {
         
         return requestURL
     }
+    
+    func getBoxOfficeData(requestURL: URLRequest, sessionConfiguration: URLSessionConfiguration, completionHandler: @escaping (BoxOffice) -> Void) {
+        let task = URLSession.shared.dataTask(with: requestURL) { data, response, error in
+            guard error == nil else {
+                return
+            }
+            
+            let successStatusRange = 200..<300
+            guard let response = response as? HTTPURLResponse,
+                  successStatusRange.contains(response.statusCode) else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                let boxOfficeData = try JSONDecoder().decode(BoxOffice.self, from: data)
+                completionHandler(boxOfficeData)
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
 }
