2ef59100e039acf9266a8766df8766a4ea35dfcf
JusBug <pksgwo123@gmail.com>
2023-07-31 11:14:37 +0900

    refactor: JSONDecodingHelper 삭제, APIManager의 decodeJSON()의 service 매개변수 제거 후 extension으로 분리

--- BoxOffice/Model/APIManager.swift
+++ BoxOffice/Model/APIManager.swift
@@ -41,11 +41,11 @@ struct APIManager {
             
             switch service {
             case .dailyBoxOffice:
-                if let decodedData: BoxOffice = self.decodeJSON(service: service, data: safeData) {
+                if let decodedData: BoxOffice = self.decodeJSON(data: safeData) {
                     print(decodedData)
                 }
             case .movieDetailInfo:
-                if let decodedData: Movie = self.decodeJSON(service: service, data: safeData) {
+                if let decodedData: Movie = self.decodeJSON(data: safeData) {
                     print(decodedData)
                 }
             }
@@ -53,8 +53,10 @@ struct APIManager {
         
         dataTask.resume()
     }
-    
-    func decodeJSON<T: Decodable>(service: Service, data: Data) -> T? {
+}
+
+extension APIManager {
+    func decodeJSON<T: Decodable>(data: Data) -> T? {
         do {
             let decoder = JSONDecoder()
             let decodedData = try decoder.decode(T.self, from: data)
