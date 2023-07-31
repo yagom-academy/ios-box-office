ff5fff662530b227db5f4f386dbb5921804c41cf
agilestarskim <agilestars@kakao.com>
2023-07-31 19:38:13 +0900

    refactor: fetchData, decode 함수 분리

--- BoxOffice/Model/Network/NetworkManager.swift
+++ BoxOffice/Model/Network/NetworkManager.swift
@@ -10,20 +10,21 @@ import Foundation
 enum FetchType {
     case boxOffice(date: String)
     case movie(code: String)
+    
+    var url: URL? {
+        switch self {
+        case .boxOffice(let date):
+            return URL(string: "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=c04de3c2ceec65d22a2c1a0b4cfe2b3c&targetDt=\(date)")
+        case .movie(let code):
+            return URL(string: "https://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=c04de3c2ceec65d22a2c1a0b4cfe2b3c&movieCd=\(code)")
+        }
+    }
 }
 
 enum NetworkManager {
     static func fetchData<T: Decodable>(fetchType: FetchType) async throws -> T {
-        var urlString = "https://kobis.or.kr/kobisopenapi/webservice/rest/"
         
-        switch fetchType {
-        case .boxOffice(date: let date):
-            urlString.append("boxoffice/searchDailyBoxOfficeList.json?key=c04de3c2ceec65d22a2c1a0b4cfe2b3c&targetDt=\(date)")
-        case .movie(code: let code):
-            urlString.append("movie/searchMovieInfo.json?key=c04de3c2ceec65d22a2c1a0b4cfe2b3c&movieCd=\(code)")
-        }
-        
-        guard let url = URL(string: urlString) else {
+        guard let url = fetchType.url else {
             throw NetworkError.invalidURL
         }
         
@@ -39,10 +40,13 @@ enum NetworkManager {
             throw NetworkError.badStatusCode(statusCode: httpResponse.statusCode)
         }
         
-        guard let decodedData = try? JSONDecoder().decode(T.self, from: data) else {
+        return try decode(from: data)
+    }
+    
+    static private func decode<T: Decodable>(from data: Data) throws -> T {
+        guard let decoded = try? JSONDecoder().decode(T.self, from: data) else {
             throw DecodingError.decodingFailed
         }
-        
-        return decodedData
+        return decoded
     }
 }
