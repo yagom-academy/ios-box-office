d85dfbbc3bbd0b411df969a25c4b0d82d4f8e239
agilestarskim <agilestars@kakao.com>
2023-07-31 20:00:15 +0900

    feat: 스토리보드 삭제 및 VC 이름 변경

---
+++ BoxOffice/Controller/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  BoxOffice
+//
+//  Created by EtialMoon, Minsup on 13/01/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad()  {
+        super.viewDidLoad()
+    }
+}
