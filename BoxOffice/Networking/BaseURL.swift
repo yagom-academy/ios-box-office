a6d8951b20d9c5cb69f30225ac7d200b4758ad28
Zion <lion2212@naver.com>
2023-07-31 19:25:14 +0900

    refactor: API Key Keys.plist에서 관리할 수 있도록 수정

--- BoxOffice/Networking/BaseURL.swift
+++ BoxOffice/Networking/BaseURL.swift
@@ -5,10 +5,6 @@
 //  Created by Hyungmin Lee on 2023/07/27.
 //
 
-enum NetworkKey {
-    static let boxOffice = "a38f2bee3ae1dc5696060047ce83c935"
-}
-
 enum BaseURL {
     static let boxOffice = "http://www.kobis.or.kr/kobisopenapi/webservice/rest"
 }
---
+++ BoxOffice/Networking/NetworkKey.swift
@@ -0,0 +1,24 @@
+//
+//  NetworkKey.swift
+//  BoxOffice
+//
+//  Created by Hyungmin Lee on 2023/07/31.
+//
+
+import Foundation
+
+enum NetworkKey {
+    static var boxOffice: String =  {
+        var boxOfficeAPIKey = ""
+        
+        if let path = Bundle.main.path(forResource: "Keys", ofType: "plist") {
+            let networkKeys = NSDictionary(contentsOfFile: path)
+            
+            if let networkKeys = networkKeys {
+                boxOfficeAPIKey = (networkKeys["BoxOfficeAPIKey"] as? String) ?? ""
+            }
+        }
+            
+        return boxOfficeAPIKey
+    }()
+}
