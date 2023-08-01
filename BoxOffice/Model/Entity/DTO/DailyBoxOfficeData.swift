960f0ce06b53794c0a428c3d1e46141793a1a55d
YaRkyungmin <kkgm94@naver.com>
2023-08-01 17:42:31 +0900

    ✨Feat: DailyBoxOfficeData 구현

---
+++ BoxOffice/Model/Entity/DTO/DailyBoxOfficeData.swift
@@ -0,0 +1,13 @@
+//
+//  DailyBoxOfficeData.swift
+//  BoxOffice
+//
+//  Created by kyungmin, Erick on 2023/08/01.
+//
+
+struct DailyBoxOfficeData {
+    let rank: String
+    let rankState: String
+    let movieTitle: String
+    let dailyAndTotalAudience: String
+}
