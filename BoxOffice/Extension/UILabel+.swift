ba393fd810e09d29c54828a85437765bfda9b02f
YaRkyungmin <kkgm94@naver.com>
2023-08-01 17:44:22 +0900

    ✨Feat: UILabel asColor 메서드 extension

---
+++ BoxOffice/Extension/UILabel+.swift
@@ -0,0 +1,18 @@
+//
+//  UILabel+.swift
+//  BoxOffice
+//
+//  Created by kyungmin, Erick on 2023/08/01.
+//
+
+import UIKit
+
+extension UILabel {
+    func asColor(targetString: String, color: UIColor) {
+        let fullText = text ?? ""
+        let attributedString = NSMutableAttributedString(string: fullText)
+        let range = (fullText as NSString).range(of: targetString)
+        attributedString.addAttribute(.foregroundColor, value: color, range: range)
+        attributedText = attributedString
+    }
+}
