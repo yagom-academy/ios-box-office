d1651b5aa4fcda0b485a9da048a71278fc13117a
Kobe <mskang@xingxingmobility.com>
2023-08-01 14:44:06 +0900

    :recycle: Refactor: API_KEY gitignore 추가.

---
+++ BoxOffice/Extension/Bundle+.swift
@@ -0,0 +1,21 @@
+//
+//  Bundle+Extension.swift
+//  BoxOffice
+//
+//  Created by Minseong Kang on 2023/08/01.
+//
+
+import Foundation
+
+extension Bundle {
+    var API: String {
+        guard let file = self.path(forResource: "API_KEY", ofType: "plist") else { return "" }
+        
+        guard let resource = NSDictionary(contentsOfFile: file) else { return "" }
+        
+        guard let apiKEY = resource["key"] as? String else {
+            fatalError("API error")
+        }
+        return apiKEY
+    }
+}
