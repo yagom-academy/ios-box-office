68145acfea879c5828cd07536edb37a1bd3f6c7b
Kobe <mskang@xingxingmobility.com>
2023-08-01 13:21:48 +0900

    :sparkles: Feat: NetworkConfigurableError 타입 구현.

---
+++ BoxOffice/Error/NetworkConfigurableError.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkConfigurableError.swift
+//  BoxOffice
+//
+//  Created by Kobe, yyss99 on 2023/08/01.
+//
+
+import Foundation
+
+enum NetworkConfigurableError: LocalizedError {
+    case urlComponents
+    case url
+    case urlResult
+}
