71dca7409ed9a4f970ea6354ed1047749883dbb5
Kobe <mskang@xingxingmobility.com>
2023-08-01 17:00:56 +0900

    :sparkles: Feat: BoxOffice 데이터 출력을 위한 함수 구현.

--- BoxOffice/Controller/ViewController.swift
+++ BoxOffice/Controller/ViewController.swift
@@ -11,9 +11,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        printBoxOfficeData()
     }
-
-
 }
 
+extension ViewController {
+    func printBoxOfficeData() {
+        do {
+            
+            let endPoint = EndPoint(
+                baseURL: "http://kobis.or.kr",
+                queryItems: [
+                    "key": "d4bb1f8d42a3b440bb739e9d49729660",
+                    "targetDt": "20230724"
+                ]
+            )
+            
+            let url = try endPoint.generateURL(
+                paths: [
+                    "/kobisopenapi",
+                    "/webservice",
+                    "/rest",
+                    "/boxoffice",
+                    "/searchDailyBoxOfficeList.json"
+                ],
+                isFullPath: false
+            )
+            
+            let urlRequest = URLRequest(url: url)
+            
+            let networkManager = NetworkManager()
+            networkManager.getBoxOfficeData(requestURL: urlRequest) { (boxOffice: BoxOffice) in
+                print(boxOffice)
+            }
+            
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
