a2ccadc9e7da465c9ee2a81fd3c14f380fae3b95
Kobe <mskang@xingxingmobility.com>
2023-08-01 10:34:07 +0900

    :sparkles: Feat: NetworkConfigurable 프로토콜 구현 및 확장.
    확장한 NetworkConfigurable 프로토콜 내부에 generateURLRequest 메서드를 구현하였습니다.

---
+++ BoxOffice/Model/NetworkConfigurable.swift
@@ -0,0 +1,36 @@
+//
+//  NetworkConfigurable.swift
+//  BoxOffice
+//
+//  Created by Kobe, yyss99 on 2023/08/01.
+//
+
+import Foundation
+
+public protocol NetworkConfigurable {
+    var baseURL: String { get }
+    var queryItems: [String: String] { get }
+}
+
+extension NetworkConfigurable {
+    public func generateURLRequest() throws -> URLRequest {
+        var urlQureyItems: [URLQueryItem] = []
+        
+        guard var urlComponents = URLComponents(string: baseURL) else {
+            throw MakeURLRequestError.convertURL
+        }
+        
+        queryItems.forEach {
+            urlQureyItems.append(URLQueryItem(name: $0.key, value: $0.value))
+        }
+        
+        urlComponents.queryItems = urlQureyItems
+        
+        guard let url = urlComponents.url else {
+            throw MakeURLRequestError.convertURL
+        }
+        let requestURL = URLRequest(url: url)
+        
+        return requestURL
+    }
+}
