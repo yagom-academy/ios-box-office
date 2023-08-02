d521a8a9a6a887acc4ae98bd018319ffd89e90b0
Min Hyun <minnimida@gmail.com>
2023-08-02 10:17:01 +0900

    feat: CollectionView 세팅 및 추가

--- BoxOffice/Controller/BoxOfficeViewController.swift
+++ BoxOffice/Controller/BoxOfficeViewController.swift
@@ -7,10 +7,34 @@
 
 import UIKit
 
+@available(iOS 14.0, *)
 final class BoxOfficeViewController: UIViewController {
+    private let collectionView: UICollectionView = {
+        let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+        let layout = UICollectionViewCompositionalLayout.list(using: configuration)
+        let view = UICollectionView(frame: .init(), collectionViewLayout: layout)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        view.backgroundColor = .systemBackground
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            collectionView.leftAnchor.constraint(equalTo: safeArea.leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: safeArea.rightAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
     }
 }
 
