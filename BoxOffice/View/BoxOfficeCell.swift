c6dbd5a60483862e9c962cebca93a5f7f35b0e43
bubblecocoa <bubblecocoa@gmail.com>
2023-08-03 21:27:49 +0900

    refactor: BoxOfficeCell이 UICollectionViewListCell를 상속받도록 변경 및 뷰 인스턴스의 제약 변경

--- BoxOffice/View/BoxOfficeCell.swift
+++ BoxOffice/View/BoxOfficeCell.swift
@@ -7,26 +7,25 @@
 
 import UIKit
 
-final class BoxOfficeCell: UICollectionViewCell {
+final class BoxOfficeCell: UICollectionViewListCell {
     static let identifier = "boxOfficeCell"
     
     // MARK: - Separator
-    private lazy var separatorLineView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .init(displayP3Red: 0.9, green: 0.9, blue: 0.9, alpha: 1)
-        view.frame.size.width = view.frame.width
-        view.translatesAutoresizingMaskIntoConstraints = false
-        
-        return view
-    }()
+    private var separatorConstraint: NSLayoutConstraint?
+    private func updateSeparatorConstraint() {
+        if let existingConstraint = separatorConstraint, existingConstraint.isActive {
+            return
+        }
+        let constraint = separatorLayoutGuide.leadingAnchor.constraint(equalTo: self.leadingAnchor)
+        constraint.isActive = true
+        separatorConstraint = constraint
+    }
     
     // MARK: - InformationStackView
     private let informationStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .horizontal
-//        stackView.spacing = 8
-        stackView.backgroundColor = .systemBackground
         
         return stackView
     }()
@@ -35,13 +34,12 @@ final class BoxOfficeCell: UICollectionViewCell {
     private let rankStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
-        stackView.spacing = 8
         stackView.alignment = .center
         
         return stackView
     }()
     
-    private let rankLabel: UILabel = {
+    let rankLabel: UILabel = {
         let label = UILabel()
         label.text = "1"
         label.font = UIFont.preferredFont(forTextStyle: .title1)
@@ -49,7 +47,7 @@ final class BoxOfficeCell: UICollectionViewCell {
         return label
     }()
     
-    private let rankInformationLabel: UILabel = {
+    let rankInformationLabel: UILabel = {
         let label = UILabel()
         label.text = "▼▲1"
         label.font = UIFont.preferredFont(forTextStyle: .body)
@@ -58,7 +56,7 @@ final class BoxOfficeCell: UICollectionViewCell {
     }()
     
     // MARK: - Movie Name, Audience Information
-    private let detailLabel: UILabel = {
+    let detailLabel: UILabel = {
         let label = UILabel()
         label.text = "TestTitle\nMoviewDetail"
         label.font = UIFont.preferredFont(forTextStyle: .title2)
@@ -68,12 +66,13 @@ final class BoxOfficeCell: UICollectionViewCell {
         return label
     }()
     
-    private let disclosureIndicatorLabel: UILabel = {
-        var label = UILabel()
-        label.text = "〉"
-        label.textColor = UIColor(displayP3Red: 0.8, green: 0.8, blue: 0.8, alpha: 1)
-        return label
-    }()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureCell()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("not implemnted")
+    }
 }
 
 // MARK: - Constraints
@@ -84,48 +83,35 @@ extension BoxOfficeCell {
     }
     
     private func configureUI() {
-        addSubview(separatorLineView)
-        
-        separatorLineView.addSubview(informationStackView)
+        addSubview(informationStackView)
         
         informationStackView.addArrangedSubview(rankStackView)
         informationStackView.addArrangedSubview(detailLabel)
-        informationStackView.addArrangedSubview(disclosureIndicatorLabel)
         
         rankStackView.addArrangedSubview(rankLabel)
         rankStackView.addArrangedSubview(rankInformationLabel)
     }
     
     private func setUpConstraints() {
-        setUpSeparatorLineViewConstraints()
+        updateSeparatorConstraint()
         setUpInformationStackViewConstraints()
         setUpRankStackViewConstraints()
         setUpDetailLabelConstraints()
-        setUpDisclosureIndicatorImageConstraints()
-    }
-    
-    private func setUpSeparatorLineViewConstraints() {
-        NSLayoutConstraint.activate([
-            separatorLineView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            separatorLineView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            separatorLineView.topAnchor.constraint(equalTo: topAnchor),
-            separatorLineView.bottomAnchor.constraint(equalTo: bottomAnchor)
-        ])
     }
     
     private func setUpInformationStackViewConstraints() {
         NSLayoutConstraint.activate([
-            informationStackView.leadingAnchor.constraint(equalTo: separatorLineView.leadingAnchor),
-            informationStackView.trailingAnchor.constraint(equalTo: separatorLineView.trailingAnchor),
-            informationStackView.topAnchor.constraint(equalTo: separatorLineView.topAnchor, constant: 1),
-            informationStackView.bottomAnchor.constraint(equalTo: separatorLineView.bottomAnchor, constant: -1)
+            informationStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            informationStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            informationStackView.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            informationStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8)
         ])
     }
     
     private func setUpRankStackViewConstraints() {
         NSLayoutConstraint.activate([
             rankStackView.leadingAnchor.constraint(equalTo: informationStackView.leadingAnchor, constant: 8),
-            rankStackView.widthAnchor.constraint(equalTo: separatorLineView.widthAnchor, multiplier: 0.15),
+            rankStackView.widthAnchor.constraint(equalTo: informationStackView.widthAnchor, multiplier: 0.15),
             rankStackView.topAnchor.constraint(equalTo: informationStackView.topAnchor, constant: -1)
         ])
     }
@@ -133,16 +119,8 @@ extension BoxOfficeCell {
     private func setUpDetailLabelConstraints() {
         NSLayoutConstraint.activate([
             detailLabel.leadingAnchor.constraint(equalTo: informationStackView.trailingAnchor, constant: 4),
-            detailLabel.centerYAnchor.constraint(equalTo: separatorLineView.centerYAnchor),
-            detailLabel.trailingAnchor.constraint(equalTo: disclosureIndicatorLabel.leadingAnchor)
-        ])
-    }
-    
-    private func setUpDisclosureIndicatorImageConstraints() {
-        NSLayoutConstraint.activate([
-            disclosureIndicatorLabel.widthAnchor.constraint(equalTo: separatorLineView.widthAnchor, multiplier: 0.08),
-            disclosureIndicatorLabel.centerYAnchor.constraint(equalTo: detailLabel.centerYAnchor),
-            disclosureIndicatorLabel.trailingAnchor.constraint(equalTo: separatorLineView.trailingAnchor, constant: 8)
+            detailLabel.centerYAnchor.constraint(equalTo: informationStackView.centerYAnchor),
+            detailLabel.trailingAnchor.constraint(equalTo: informationStackView.leadingAnchor)
         ])
     }
 }
