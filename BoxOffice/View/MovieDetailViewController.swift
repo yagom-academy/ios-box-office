db16831602e521daf6f6be99ef75a0180275abc9
h-suo <phstoo1021@naver.com>
2023-08-05 17:43:42 +0900

    ✨Feat: MovieDetailVC에 movieCode 추가 및 init 생성

--- BoxOffice/View/MovieDetailViewController.swift
+++ BoxOffice/View/MovieDetailViewController.swift
@@ -8,7 +8,18 @@
 import UIKit
 
 class MovieDetailViewController: UIViewController {
+    let movieCode: String
     let movieDetailView = MovieDetailView()
+    
+    init(movieCode: String) {
+        self.movieCode = movieCode
+        
+        super.init()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func loadView() {
         self.view = movieDetailView
