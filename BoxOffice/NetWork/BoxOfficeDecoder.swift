d97a51955fa01fc182808314a036d4cc7e59d84c
karen <karenrodanthe@icloud.com>
2023-08-06 17:19:56 +0900

    ✴️feat: 서버에서 파일 받아오고 Decoding할 BoxOfficeDecoder 구현

--- BoxOffice/NetWork/BoxOfficeDecoder.swift
+++ BoxOffice/NetWork/BoxOfficeDecoder.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+final class BoxOfficeDecoder<Element: Decodable> {
+    private let apiType: KobisAPIType
+    private let model: NetworkingProtocol
+    private var task: URLSessionDataTask?
+
+    init(apiType: KobisAPIType, model: NetworkingProtocol) {
+        self.apiType = apiType
+        self.model = model
+    }
+
+    private func decodeData(_ type: Element.Type, from data: Data) -> Element? {
+        let decoder: JSONDecoder = JSONDecoder()
+        return try? decoder.decode(type, from: data)
+    }
+
+    func fetchData(handler: @escaping (Result<Element, BoxOfficeError>) -> Void) {
+        guard let url = apiType.receiveUrl() else {
+            handler(.failure(.urlError))
+            return
+        }
+
+        task = model.getRequest(url: url) { [weak self] result in
+            switch result {
+            case .success(let data):
+                guard let decodingData = self?.decodeData(Element.self, from: data) else {
+                    handler(.failure(.decodingFail))
+                    return
+                }
+                handler(.success(decodingData))
+            case .failure(let error):
+                handler(.failure(error))
+            }
+        }
+    }
+}
--- BoxOffice/NetWork/KobisAPIType.swift
+++ BoxOffice/NetWork/KobisAPIType.swift
@@ -6,3 +6,61 @@
 //
 
 import Foundation
+
+enum KobisAPIType {
+    case movie(String)
+    case boxOffice(String)
+}
+
+extension KobisAPIType {
+    private var apiKey: String {
+        switch self {
+        case .movie, .boxOffice:
+            return Bundle.main.apiKey
+        }
+    }
+    
+    private var scheme: String {
+        switch self {
+        case .movie, .boxOffice:
+            return "http"
+        }
+    }
+    
+    private var host: String {
+        switch self {
+        case .movie, .boxOffice:
+            return "kobis.or.kr"
+        }
+    }
+    
+    private var path: String {
+        switch self {
+        case .movie:
+            return "/kobisopenapi/webservice/rest/movie/searchMovieInfo.json"
+        case .boxOffice:
+            return "/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
+        }
+    }
+    
+    private var queries: [URLQueryItem] {
+        switch self {
+        case .movie(let code):
+            return [URLQueryItem(name: "key", value: apiKey),
+                    URLQueryItem(name: "movieCd", value: code)]
+        case .boxOffice(let date):
+            return [URLQueryItem(name: "key", value: apiKey),
+                    URLQueryItem(name: "targetDt", value: date)]
+        }
+    }
+    
+    func receiveUrl() -> URL? {
+        let url = URL(scheme, host, path, queries)
+        switch self {
+        case .movie(_):
+            return url
+        case .boxOffice(_):
+            return url
+        }
+    }
+}
