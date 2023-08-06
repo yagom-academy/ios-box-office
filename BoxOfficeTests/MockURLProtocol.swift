c6a2eee4f2bd75248caee65f6a3a5411fcaa3763
karen <karenrodanthe@icloud.com>
2023-08-06 17:34:17 +0900

    ✅ URL의 mock데이터 구현

---
+++ BoxOfficeTests/MockURLProtocol.swift
@@ -0,0 +1,40 @@
+//
+//  MockURLProtocol.swift
+//  BoxOfficeTests
+//
+//  Created by karen on 2023/08/06.
+//
+
+import Foundation
+
+final class MockURLProtocol: URLProtocol {
+
+    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data))?
+
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+
+    override func startLoading() {
+        guard let handler = MockURLProtocol.requestHandler else {
+            fatalError("handler가 응답이 없습니다.")
+        }
+
+        do {
+            let (response, data) = try handler(request)
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            client?.urlProtocol(self, didLoad: data)
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+
+    }
+
+    override func stopLoading() {}
+
+}
