cbf1baabbfac009be5fe1c3f10ede89eaf57f8cd
Zion <lion2212@naver.com>
2023-08-07 19:04:33 +0900

    feat: MovieInformationDTO Test 로직 추가

--- MovieInformationDtoTests/MovieInformationDTOTests.swift
+++ MovieInformationDtoTests/MovieInformationDTOTests.swift
@@ -37,4 +37,77 @@ final class MovieInformationDTOTests: XCTestCase {
         XCTAssertEqual(audienceCountExpectation, audienceCountResult)
         XCTAssertEqual(audienceAccumulateExpectation, audienceAccumulateResult)
     }
+    
+    func test_OldAndNew가_New로되있을시_신작으로표시되는지_확인() {
+        //given
+        let expectiontation = addAttributeString(text: "신작", keyword: "신작", color: .red)
+        
+        //when
+        let result = sut.conventedRankIntenSybolAndText()
+        
+        //then
+        XCTAssertEqual(expectiontation, result)
+    }
+    
+    func test_OldAndNew가_OLD로되있을시_상승rank로_표시되는지_확인() {
+        //given
+        sut = MovieInformationDTO(rank: "1",
+                                  rankInten: "1",
+                                  oldAndNew: "OLD",
+                                  movieName: "밀수",
+                                  audienceCount: "450000",
+                                  audienceAccumulate: "6000000")
+        
+        let expectiontation = addAttributeString(text: "▲\(sut.rankInten)", keyword: "▲", color: .red)
+        
+        //when
+        let result = sut.conventedRankIntenSybolAndText()
+        
+        //then
+        XCTAssertEqual(expectiontation, result)
+    }
+    
+    func test_OldAndNew가_OLD로되있을시_하락rank로_표시되는지_확인() {
+        //given
+        sut = MovieInformationDTO(rank: "10",
+                                  rankInten: "-3",
+                                  oldAndNew: "OLD",
+                                  movieName: "밀수",
+                                  audienceCount: "450000",
+                                  audienceAccumulate: "6000000")
+        
+        let decreaseRank = sut.rankInten.replacingOccurrences(of: "-", with: "")
+        let expectiontation = addAttributeString(text: "▼\(decreaseRank)", keyword: "▼", color: .blue)
+        
+        //when
+        let result = sut.conventedRankIntenSybolAndText()
+        
+        //then
+        XCTAssertEqual(expectiontation, result)
+    }
+    
+    func test_OldAndNew가_OLD로되있을시_동일Rank로_표시되는지_확인() {
+        //given
+        sut = MovieInformationDTO(rank: "10",
+                                  rankInten: "0",
+                                  oldAndNew: "OLD",
+                                  movieName: "밀수",
+                                  audienceCount: "450000",
+                                  audienceAccumulate: "6000000")
+        
+        let expectiontation = addAttributeString(text: "-", keyword: "-", color: .black)
+        
+        //when
+        let result = sut.conventedRankIntenSybolAndText()
+        
+        //then
+        XCTAssertEqual(expectiontation, result)
+    }
+    
+    private func addAttributeString(text: String, keyword: String, color: UIColor) -> NSMutableAttributedString {
+        let attributeString = NSMutableAttributedString(string: text)
+        
+        attributeString.addAttribute(.foregroundColor, value: color , range: (text as NSString).range(of: keyword))
+        return attributeString
+    }
 }
