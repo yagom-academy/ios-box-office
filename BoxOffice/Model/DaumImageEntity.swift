e73303fb70f3737c46b308b60cac6b0db39a6bfd
iOS-Yetti <yechan123456@gmail.net>
2023-08-08 09:44:05 +0900

    feat: DaumImageEntity 파일 추가 및 DAUM_API_KEY 추가

---
+++ BoxOffice/Model/DaumImageEntity.swift
@@ -0,0 +1,41 @@
+//
+//  DaumImageEntity.swift
+//  BoxOffice
+//
+//  Created by Yetti, Maxhyunm on 2023/08/08.
+//
+import Foundation
+
+struct DaumImageEntity: Decodable {
+    let meta: Meta
+    let documents: [Document]
+}
+
+extension DaumImageEntity {
+    struct Meta: Decodable {
+        let totalCount, pageableCount: Int
+        let isEnd: Bool
+        
+        enum CodingKeys: String, CodingKey {
+            case totalCount = "total_count"
+            case pageableCount = "pageable_count"
+            case isEnd = "is_end"
+        }
+    }
+}
+
+extension DaumImageEntity {
+    struct Document: Decodable {
+        let collection, thumbnailURL, imageURL, displaySitename, documentURL: String
+        let width, height: Int
+        let datetime: Date
+        
+        enum CodingKeys: String, CodingKey {
+            case collection, width, height, datetime
+            case thumbnailURL = "thumbnail_url"
+            case imageURL = "image_url"
+            case displaySitename = "display_sitename"
+            case documentURL = "doc_url"
+        }
+    }
+}
