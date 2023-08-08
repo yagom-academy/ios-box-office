a798ae8d4c6e29ff0ad7558779000ec7696dec5c
Kobe <mskang@xingxingmobility.com>
2023-08-08 21:01:06 +0900

    :recycle: Refactor: configureDataSource, creatLayout 함수 구현
    collection view layout 및 data source 구현을 위한 베이스 작업 진행중.

--- BoxOffice/Controller/ViewController.swift
+++ BoxOffice/Controller/ViewController.swift
@@ -8,10 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var itemData: [DailyBoxOfficeList] = []
+    let networkManager: NetworkManager = NetworkManager()
+    var dataSource: UICollectionViewDiffableDataSource<Section, DailyBoxOfficeList>? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         printBoxOfficeData()
+    }
+    
+    func creatLayout() -> UICollectionViewLayout {
+        let config = UICollectionLayoutListConfiguration(appearance: .plain)
+        return UICollectionViewCompositionalLayout.list(using: config)
+    }
+    
+    lazy var collectionView: UICollectionView = UICollectionView(frame: view.bounds, collectionViewLayout: creatLayout())
+    
+    func configureHierarchy() {
+        collectionView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        view.addSubview(collectionView)
+    }
+    
+    func configureDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<CustomListCell, DailyBoxOfficeList> { (cell, indexPath, item) in
+            cell.updateWithItem(item)
+            cell.accessories = [.disclosureIndicator()]
+        }
+        dataSource = UICollectionViewDiffableDataSource<Section, DailyBoxOfficeList>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, itemIdentifier) -> UICollectionViewCell in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        })
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, DailyBoxOfficeList>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(itemData)
+        guard let dataSource = dataSource else { return }
+        dataSource.apply(snapshot)
     }
 }
 
