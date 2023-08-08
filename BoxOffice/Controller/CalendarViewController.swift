43fffca409de71dee87ca269527c760f10c327cb
redmango1446@gmail.com <redmango1446>
2023-08-08 11:33:55 +0900

    feat: CalenderView 생성 및 초기 구현

--- BoxOffice/Controller/CalendarViewController.swift
+++ BoxOffice/Controller/CalendarViewController.swift
@@ -8,8 +8,18 @@
 import UIKit
 
 class CalendarViewController: UIViewController {
+    var calendarView: UICalendarView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        showCalendarView()
+    }
+    
+    private func showCalendarView() {
+        calendarView = UICalendarView(frame: CGRect(x: 0, y: 0,
+                                                    width: view.bounds.width,
+                                                    height: view.bounds.height))
+        calendarView.backgroundColor = .white
+        view.addSubview(calendarView)
     }
 }
