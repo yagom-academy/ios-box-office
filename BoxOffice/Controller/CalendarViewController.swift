a26b26aae09879ec051c9e244cefbe6de8731a71
JusBug <pksgwo123@gmail.com>
2023-08-08 11:24:14 +0900

    feat: 날짜 선택 button 생성, present modally으로 popover 구현

---
+++ BoxOffice/Controller/CalendarViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CalendarViewController.swift
+//  BoxOffice
+//
+//  Created by 박종화 on 2023/08/08.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- BoxOffice/Controller/MainViewController.swift
+++ BoxOffice/Controller/MainViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 class MainViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var loadingActivityView: UIActivityIndicatorView!
+    @IBOutlet weak var calendarButton: UIButton!
     var boxOffice: BoxOffice?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         showLodingView()
@@ -30,6 +31,12 @@ class MainViewController: UIViewController {
     private func assignDataSourceAndDelegate() {
         collectionView.dataSource = self
         collectionView.delegate = self
+    }
+    
+    @IBAction func tabCalendarButton(_ sender: UIButton) {
+        let calendarVC = CalendarViewController()
+        calendarVC.modalPresentationStyle = .popover
+        self.present(calendarVC, animated: true, completion: nil)
     }
     
     private func initRefresh() {
