144888cb83c18dd085176c42f9272c1c6c743d15
bubblecocoa <bubblecocoa@gmail.com>
2023-08-09 14:06:01 +0900

    feat: MovieDetailNameSpace 생성

---
+++ BoxOffice/NameSpace/MovieDetailNameSpace.swift
@@ -0,0 +1,16 @@
+//
+//  MovieDetailNameSpace.swift
+//  BoxOffice
+//
+//  Created by Serena, BMO on 2023/08/09.
+//
+
+enum MovieDetailNameSpace {
+    static let directors: String = "감독"
+    static let productionYear: String = "제작년도"
+    static let openDate: String = "개봉일"
+    static let showTime: String = "상영시간"
+    static let audits: String = "관람등급"
+    static let nations: String = "제작국가"
+    static let genres: String = "장르"
+}
