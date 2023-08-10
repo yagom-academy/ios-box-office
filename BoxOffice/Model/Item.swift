71a432f174fe2be39e64104c3d1f407b17de7866
yyss99 <yyssjj9999@gmail.com>
2023-08-10 19:17:10 +0900

    ✨feat: Item 타입 생성

---
+++ BoxOffice/Model/Item.swift
@@ -0,0 +1,28 @@
+//
+//  Item.swift
+//  BoxOffice
+//
+//  Created by Kobe, yyss99 on 2023/08/10.
+//
+
+import Foundation
+
+struct Item: Hashable {
+    let rankNumber: String?
+    let rankIntensity: String?
+    let movieName: String?
+    let audienceCount: String?
+    let audienceAccumulated: String?
+    
+    init(rankNumber: String? = nil, rankIntensity: String? = nil, movieName: String? = nil, audienceCount: String? = nil, audienceAccumulated: String? = nil) {
+        self.rankNumber = rankNumber
+        self.rankIntensity = rankIntensity
+        self.movieName = movieName
+        self.audienceCount = audienceCount
+        self.audienceAccumulated = audienceAccumulated
+    }
+    
+    private let identifier = UUID()
+    
+    static var all: [Item] = []
+}
