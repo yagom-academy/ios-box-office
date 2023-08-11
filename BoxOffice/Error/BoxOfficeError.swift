dce961528add3780994c191872a25443ac383d8f
karen <karenrodanthe@icloud.com>
2023-08-12 01:33:21 +0900

    🖨️chore: Error enum 네이밍 변경

--- BoxOffice/Error/BoxOfficeError.swift
+++ BoxOffice/Error/BoxOfficeError.swift
@@ -7,31 +7,31 @@
 import Foundation
 
 enum BoxOfficeError {
-    case urlError
-    case requestFail
-    case responseFail
+    case invalidURL
+    case failureRequest
+    case failureReseponse
     case unknownError
-    case typeError
-    case decodingFail
-    case APIKeyError
+    case invalidType
+    case failureDecoding
+    case invalidAPIKey
 }
 
 extension BoxOfficeError: LocalizedError {
     var errorDescription: String? {
         switch self {
-        case .urlError:
+        case .invalidURL:
             return "url형식이 잘못되었습니다"
-        case .requestFail:
+        case .failureRequest:
             return "데이터 요청에 실패했습니다."
-        case .responseFail:
+        case .failureReseponse:
             return "응답이 없습니다."
         case .unknownError:
             return "알 수 없는 에러입니다."
-        case .typeError:
+        case .invalidType:
             return "올바르지 않는 데이터 포맷입니다"
-        case .decodingFail:
+        case .failureDecoding:
             return "데이터를 변환하지 못하였습니다"
-        case .APIKeyError:
+        case .invalidAPIKey:
             return "APIKEY 설정 값이 누락되었거나 잘못된 키값입니다."
         }
     }
