68359d4d100014539a832ebd862eb3cb404f1ea8
karen <karenrodanthe@icloud.com>
2023-08-12 01:36:08 +0900

    🖨️chore: Bundle guard문 개행 수정

--- BoxOffice/Extension/Bundle+.swift
+++ BoxOffice/Extension/Bundle+.swift
@@ -1,5 +1,5 @@
 //
-//  Bundle.swift
+//  Bundle+.swift
 //  BoxOffice
 //
 //  Created by karen on 2023/08/06.
@@ -11,7 +11,10 @@ extension Bundle {
     var apiKey: String {
         guard let file = path(forResource: "APIKEY", ofType: "plist"),
               let resource = NSDictionary(contentsOfFile: file),
-              let key = resource["KobisAPIKey"] as? String else { fatalError("\(BoxOfficeError.APIKeyError)") }
+              let key = resource["KobisAPIKey"] as? String else {
+            fatalError("\(BoxOfficeError.invalidAPIKey)")
+        }
+        
         return key
     }
 }
