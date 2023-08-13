80de1f2f4d2e1f34ccdba055a5184c933a4f916f
Kobe <mskang@xingxingmobility.com>
2023-08-13 10:12:55 +0900

    :sparkles: Feat: DetailViewController 생성.

---
+++ BoxOffice/Controller/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  BoxOffice
+//
+//  Created by Kobe, yyss99 on 2023/08/13.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemPink
+    }
+
+}
