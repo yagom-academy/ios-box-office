59689df65121cc5ca038a16a53a0c047e4b0a367
karen <karenrodanthe@icloud.com>
2023-08-15 00:30:43 +0900

    ✴️ feat: BoxOfficeViewController에 컬렉션뷰 관련 메서드 구현

--- BoxOffice/Controller/BoxOfficeViewController.swift
+++ BoxOffice/Controller/BoxOfficeViewController.swift
@@ -31,6 +31,7 @@ final class BoxOfficeViewController: UIViewController, UICollectionViewDelegate 
         createDataManager()
         fetchBoxOfficeData()
         startLoadingView()
+        configureUI()
     }
     
     private func createDataManager() {
@@ -47,6 +48,7 @@ final class BoxOfficeViewController: UIViewController, UICollectionViewDelegate 
             DispatchQueue.main.async {
                 self.stopLoadingView()
                 self.collectionView?.refreshControl?.endRefreshing()
+                self.applySnapshot(section: self.rankingViewType)
             }
         case .failure(let error):
             presentErrorAlert(error: error, title: "박스오피스")
@@ -69,4 +71,97 @@ final class BoxOfficeViewController: UIViewController, UICollectionViewDelegate 
         collectionView.deselectItem(at: indexPath, animated: true)
     }
     
+}
+
+// MARK: UI
+private extension BoxOfficeViewController {
+    func configureUI() {
+        view.backgroundColor = .systemBackground
+        
+        let layout = makeCollectionViewListLayout()
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        configureNavigationTitle()
+        configureCollectionViewLayout()
+        configureLoadingView()
+        configureRefreshController()
+    }
+    
+    func configureNavigationTitle() {
+        navigationItem.title = dataManager?.navigationTitleText
+    }
+    
+    func makeCollectionViewListLayout() -> UICollectionViewCompositionalLayout {
+        let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+        let layout = UICollectionViewCompositionalLayout.list(using: configuration)
+        
+        return layout
+    }
+    
+    func configureCollectionViewLayout() {
+        guard let collectionView = collectionView else { return }
+        
+        view.addSubview(collectionView)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.delegate = self
+        
+        addConstraintCollectionView(collectionView)
+        createDataSource(collectionView)
+    }
+    
+    func addConstraintCollectionView(_ collectionView: UICollectionView) {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    func configureLoadingView() {
+        loadingView.center = view.center
+        loadingView.style = .large
+        
+        view.addSubview(loadingView)
+    }
+    
+    func configureRefreshController() {
+        refreshController.addTarget(self, action: #selector(refreshCollectionView), for: .valueChanged)
+        collectionView?.refreshControl = refreshController
+    }
+
+    func createDataSource(_ collectionView: UICollectionView) {
+        let listCellRegistration = makeListCellRegistration()
+        
+        dataSource = UICollectionViewDiffableDataSource<BoxOfficeRankingViewType, BoxOfficeMovieInfo>(
+            collectionView: collectionView,
+            cellProvider: { collectionView, indexPath, itemIdentifier in
+                return collectionView.dequeueConfiguredReusableCell(
+                    using: listCellRegistration,
+                    for: indexPath,
+                    item: itemIdentifier
+                )
+            }
+        )
+    }
+    
+    func makeListCellRegistration() -> UICollectionView.CellRegistration<MovieRankingListCell, BoxOfficeMovieInfo> {
+        return UICollectionView.CellRegistration<MovieRankingListCell, BoxOfficeMovieInfo> { cell, indexPath, item in
+            let uiModel = CellUIModel(data: item)
+            cell.updateLabelText(for: uiModel)
+        }
+    }
+    
+    func applySnapshot(section: BoxOfficeRankingViewType?) {
+        guard let dataManager = dataManager,
+              let section = section else { return }
+        
+        var snapshot = NSDiffableDataSourceSnapshot<BoxOfficeRankingViewType, BoxOfficeMovieInfo>()
+        
+        snapshot.appendSections([section])
+        snapshot.appendItems(dataManager.movieItems)
+        
+        dataSource?.apply(snapshot, animatingDifferences: true)
+    }
 }
