bbc598d34f89ac223fb50300138ffd07cee63c7d
karen <karenrodanthe@icloud.com>
2023-08-14 23:42:54 +0900

    🖨️chore: DefaultDecodingService 컨벤션 수정

--- BoxOffice/NetWork/DefaultDecodingService.swift
+++ BoxOffice/NetWork/DefaultDecodingService.swift
@@ -10,6 +10,7 @@ import Foundation
 struct DefaultDecodingService: DecodingService {
     func decode<Element: Decodable>(_ type: Element.Type, from data: Data) -> Element? {
         let decoder = JSONDecoder()
+       
         return try? decoder.decode(type, from: data)
     }
 }
