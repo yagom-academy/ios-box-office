18d1f8fb7a8f308a843d20b899953f8905031f3c
redmango1446@gmail.com <redmango1446>
2023-08-14 18:06:07 +0900

    refactor: fatalError발생 제거

--- BoxOffice/Controller/CalendarViewController.swift
+++ BoxOffice/Controller/CalendarViewController.swift
@@ -19,7 +19,7 @@ final class CalendarViewController: UIViewController {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
     
     override func viewDidLoad() {
