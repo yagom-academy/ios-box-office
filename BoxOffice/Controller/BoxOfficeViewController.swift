20fb2fc8028f4f2df48e1631daaacce9e459aed3
karen <karenrodanthe@icloud.com>
2023-08-15 00:28:51 +0900

    ✴️ feat: BoxOfficeViewController에 네네트워크 통신 메서드 구현

--- BoxOffice/Controller/BoxOfficeViewController.swift
+++ BoxOffice/Controller/BoxOfficeViewController.swift
@@ -7,25 +7,38 @@
 
 import UIKit
 
-final class BoxOfficeViewController: UIViewController, UICollectionViewDelegate  {
-    private var dataManager = {
-        guard let yesterday = Date.yesterday else {
-            return DataManager(date: Date())
+final class BoxOfficeViewController: UIViewController, UICollectionViewDelegate {
+    // MARK: Propertie
+    private var rankingViewType: BoxOfficeRankingViewType = .list
+    private var dataManager: DataManager?
+    private var boxofficeDate = {
+        guard let boxofficeDate = Date.yesterday else {
+            return Date()
         }
-        
-        return DataManager(date: yesterday)
+        return boxofficeDate
     }()
     
+    // MARK: UI Properties
     private let loadingView = UIActivityIndicatorView()
+    private let refreshController = UIRefreshControl()
     private var collectionView: UICollectionView?
+    
+    // MARK: DataSource Properties
+    private var dataSource: UICollectionViewDiffableDataSource<BoxOfficeRankingViewType, BoxOfficeMovieInfo>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        createDataManager()
         fetchBoxOfficeData()
+        startLoadingView()
+    }
+    
+    private func createDataManager() {
+        dataManager = DataManager(date: boxofficeDate)
     }
     
     private func fetchBoxOfficeData() {
-        dataManager.fetchRanking(handler: handleFetchResult)
+        dataManager?.fetchRanking(handler: handleFetchResult)
     }
     
     private func handleFetchResult(result: Result<[BoxOfficeMovieInfo], Error>) {
@@ -46,6 +59,14 @@ final class BoxOfficeViewController: UIViewController, UICollectionViewDelegate 
     
     private func stopLoadingView() {
         self.loadingView.stopAnimating()
+    }
+    
+    @objc private func refreshCollectionView() {
+        self.fetchBoxOfficeData()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
     }
     
 }
