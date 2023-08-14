01769911bba4bdc309f2a042a07a54272a604d70
karen <karenrodanthe@icloud.com>
2023-08-14 23:36:03 +0900

    ✴️feat: numberFormatter 구현

---
+++ BoxOffice/Extension/String+.swift
@@ -0,0 +1,24 @@
+//
+//  String+.swift
+//  BoxOffice
+//
+//  Created by karen on 2023/08/14.
+//
+
+import Foundation
+
+extension String {
+    static let numberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        
+        return formatter
+    }()
+    
+    func formatDecimal() -> String? {
+        guard let value = Int(self),
+              let formattedNumber = String.numberFormatter.string(from: NSNumber(value: value)) else { return nil }
+        
+        return formattedNumber
+    }
+}
