258bf09db225a8a04e63f8fdf364def0f8a3ac44
karen <karenrodanthe@icloud.com>
2023-08-14 23:30:27 +0900

    💥refactor: 스토리보드 삭제 후 코드베이스 준비

--- BoxOffice/Application/SceneDelegate.swift
+++ BoxOffice/Application/SceneDelegate.swift
@@ -12,7 +12,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UINavigationController(rootViewController: BoxOfficeViewController())
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
