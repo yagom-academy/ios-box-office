9c357fcca21d22187ade74825088214f3d393dfd
Hisop <khsdfg958@gmail.com>
2023-11-27 22:18:25 +0900

    feat: 타입 구현
    약어x

---
+++ BoxOffice/Model/BoxOfficeResult.swift
@@ -0,0 +1,54 @@
+//
+//  File.swift
+//  BoxOffice
+//
+//  Created by Hisop on 2023/11/27.
+//
+
+struct BoxOfficeResult: Decodable {
+    let boxofficeType: String
+    let showRange: String
+    let dailyBoxOfficeList: [MovieStatistics]
+}
+
+struct MovieStatistics: Decodable {
+    let rankNumber: String
+    let rank: String
+    let rankIntensity: String
+    let rankOldAndNew: String
+    let movieCode: String
+    let movieName: String
+    let openDate: String
+    let salesAmount: String
+    let salesShare: String
+    let salesIntensity: String
+    let salesChange: String
+    let salesAccumulated: String
+    let audienceCount: String
+    let audienceIntensity: String
+    let audienceChange: String
+    let audienceAccumulated: String
+    let screenCount: String
+    let showCount: String
+    
+    enum CodingKeys: String, CodingKey {
+        case rankNumber = "rnum"
+        case rank
+        case rankIntensity = "rankInten"
+        case rankOldAndNew
+        case movieCode = "movieCd"
+        case movieName = "movieNm"
+        case openDate = "openDt"
+        case salesAmount = "salesAmt"
+        case salesShare
+        case salesIntensity = "salesInten"
+        case salesChange
+        case salesAccumulated = "salesAcc"
+        case audienceCount = "audiCnt"
+        case audienceIntensity = "audiInten"
+        case audienceChange = "audiChange"
+        case audienceAccumulated = "audiAcc"
+        case screenCount = "scrnCnt"
+        case showCount = "showCNt"
+    }
+}
