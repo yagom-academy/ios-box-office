8cd76b15b2401a3aa6ae04a0deb79aa044cc6c20
kiseok <carti1108@naver.com>
2023-11-27 16:47:41 +0900

    test: json모델 파싱 테스트

--- MovieTests/MovieTests.swift
+++ MovieTests/MovieTests.swift
@@ -10,7 +10,8 @@ import XCTest
 
 final class MovieTests: XCTestCase {
 
-    func test_Movie() {
+    func test_init을통해만든Movie타입과Json데이터를_비교했을때_서로같다() {
+        //given
         let boxOfficeType = "일별 박스오피스"
         let showRange = "20220105~20220105"
         let number = "1"
@@ -63,12 +64,74 @@ final class MovieTests: XCTestCase {
         }
         """
         
-        let dailyBoxOfficeList = DailyBoxOfficeList(number: number, rank: rank, rankFluctuation: rankFluctuation, rankOldAndNew: rankOldAndNew, movieCode: movieCode, movieName: movieName, openDate: openDate, salesAmount: salesAmount, salesShare: salesShare, salesFluctuation: salesFluctuation, salesChange: salesChange, salesAccumulation: salesAccumulation, audienceCount: audienceCount, audienceFluctuation: audienceFluctuation, audienceChange: audienceChange, audienceAccumulation: audienceAccumulation, screenCount: screenCount, showCount: showCount)
-        let boxOfficeResult = BoxOfficeResult(boxOfficeType: boxOfficeType, showRange: showRange, dailyBoxOfficeList: [dailyBoxOfficeList])
+        let dailyBoxOfficeList = DailyBoxOfficeList(
+            number: number,
+            rank: rank,
+            rankFluctuation: rankFluctuation,
+            rankOldAndNew: rankOldAndNew,
+            movieCode: movieCode,
+            movieName: movieName,
+            openDate: openDate,
+            salesAmount: salesAmount,
+            salesShare: salesShare,
+            salesFluctuation: salesFluctuation,
+            salesChange: salesChange,
+            salesAccumulation: salesAccumulation,
+            audienceCount: audienceCount,
+            audienceFluctuation: audienceFluctuation,
+            audienceChange: audienceChange,
+            audienceAccumulation: audienceAccumulation,
+            screenCount: screenCount,
+            showCount: showCount
+        )
+        
+        let boxOfficeResult = BoxOfficeResult(
+            boxOfficeType: boxOfficeType,
+            showRange: showRange,
+            dailyBoxOfficeList: [dailyBoxOfficeList]
+        )
+        
         let movie = Movie(boxOfficeResult: boxOfficeResult)
-
-        guard let data = MovieTestData.json.data(using: .utf8) else { return }
-        let jsonData = try? JSONDecoder().decode(Movie.self, from: data)
+        let data = json.data(using: .utf8)!
+        let jsonData = try! JSONDecoder().decode(Movie.self, from: data)
+        
+        //when, then
+        XCTAssertEqual(movie, jsonData)
     }
+}
 
+extension DailyBoxOfficeList: Equatable {
+    public static func == (lhs: DailyBoxOfficeList, rhs: DailyBoxOfficeList) -> Bool {
+        return lhs.number == rhs.number &&
+        lhs.rank == rhs.rank &&
+        lhs.rankFluctuation == rhs.rankFluctuation &&
+        lhs.rankOldAndNew == rhs.rankOldAndNew &&
+        lhs.movieCode == rhs.movieCode &&
+        lhs.openDate == rhs.openDate &&
+        lhs.salesAmount == rhs.salesAmount &&
+        lhs.salesShare == rhs.salesShare &&
+        lhs.salesFluctuation == rhs.salesFluctuation &&
+        lhs.salesChange == rhs.salesChange &&
+        lhs.salesAccumulation == rhs.salesAccumulation &&
+        lhs.audienceCount == rhs.audienceCount &&
+        lhs.audienceFluctuation == rhs.audienceFluctuation &&
+        lhs.audienceChange == rhs.audienceChange &&
+        lhs.audienceAccumulation == rhs.audienceAccumulation &&
+        lhs.screenCount == rhs.screenCount &&
+        lhs.showCount == rhs.showCount
+    }
+}
+
+extension BoxOfficeResult: Equatable {
+    public static func == (lhs: BoxOfficeResult, rhs: BoxOfficeResult) -> Bool {
+        return lhs.boxOfficeType == rhs.boxOfficeType &&
+        lhs.showRange == rhs.showRange &&
+        lhs.dailyBoxOfficeList == rhs.dailyBoxOfficeList
+    }
+}
+
+extension Movie: Equatable {
+    public static func == (lhs: Movie, rhs: Movie) -> Bool {
+        return lhs.boxOfficeResult == rhs.boxOfficeResult
+    }
 }
