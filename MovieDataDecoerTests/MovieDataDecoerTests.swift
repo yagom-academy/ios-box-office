56f1a9de0879ce0e3a72ed70ff48c6547414a452
Toy <woohyeon3382@naver.com>
2023-11-27 17:53:13 +0900

    feat: MovieDataDecoerTests 파일 생성

---
+++ MovieDataDecoerTests/MovieDataDecoerTests.swift
@@ -0,0 +1,24 @@
+//
+//  MovieDataDecoerTests.swift
+//  MovieDataDecoerTests
+//
+//  Created by Morgan, Toy on 11/27/23.
+//
+
+import XCTest
+@testable import BoxOffice
+
+final class MovieDataDecoerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+    
+    }
+}
