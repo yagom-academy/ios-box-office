fe834d4c1bccd51f4fe68d3c374ccb9fd1fd106b
Toy <woohyeon3382@naver.com>
2023-11-28 17:52:58 +0900

    refactor: BoxOffice 타입과 BoxOfficeResult 타입 분리

--- MovieDataDecoerTests/MovieDataDecoerTests.swift
+++ MovieDataDecoerTests/MovieDataDecoerTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import BoxOffice
 
 final class MovieDataDecoerTests: XCTestCase {
-    private var sut: BoxOfficeResult!
+    private var sut: BoxOffice!
     
     override func setUpWithError() throws {
         try super.setUpWithError()
@@ -21,10 +21,10 @@ final class MovieDataDecoerTests: XCTestCase {
         sut = nil
     }
 
-    private func test_box_office_sample_디코딩후_boxofficeType은_일별_박스오피스를_반환한다() {
+    func test_box_office_sample_디코딩후_boxofficeType은_일별_박스오피스를_반환한다() {
         // given
-        let result = sut.boxofficeType
-        
+        let result = sut.boxOfficeResult.boxOfficeType
+        print(result)
         // when
         let boxofficeType = "일별 박스오피스"
     
@@ -32,9 +32,9 @@ final class MovieDataDecoerTests: XCTestCase {
         XCTAssertEqual(result, boxofficeType)
     }
     
-    private func test_box_office_sample_디코딩후_dailyBoxOfficeList의_0번째요소의_movieName가_경관의_피_를반환한다() {
+    func test_box_office_sample_디코딩후_dailyBoxOfficeList의_0번째요소의_movieName가_경관의_피_를반환한다() {
         // given
-        let result = sut.dailyBoxOfficeList[0].movieName
+        let result = sut.boxOfficeResult.dailyBoxOfficeList[0].movieName
 
         // when
         let movieName = "경관의 피"
