a44c9fe7d9b9f4a6cff831bc61930814c323cac3
kiseok <carti1108@naver.com>
2023-11-29 11:02:23 +0900

    refactor: 강제 언래핑 제거

--- MovieTests/MovieTests.swift
+++ MovieTests/MovieTests.swift
@@ -9,15 +9,22 @@ import XCTest
 @testable import BoxOffice
 
 final class MovieTests: XCTestCase {
-
+    
     func test_init을통해만든Movie타입과Json데이터를_비교했을때_서로같다() {
         //given
         let movie = DummyMovie().movie
-        let data = TestMovieJsonData.json.data(using: .utf8)!
-        let jsonData = try! JSONDecoder().decode(Movie.self, from: data)
+        
+        guard let data = TestMovieJsonData.json.data(using: .utf8) else {
+            return XCTFail()
+        }
         
         //when, then
-        XCTAssertEqual(movie, jsonData)
+        do {
+            let jsonData = try JSONDecoder().decode(Movie.self, from: data)
+            XCTAssertEqual(movie, jsonData)
+        } catch  {
+            XCTFail()
+        }
     }
 }
 
