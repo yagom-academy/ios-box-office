98c778743ef0ce04a6a52998bc1a7c46e187f1a2
jyubong <nane030@naver.com>
2023-11-30 15:53:37 +0900

    feat: 네트워크를 담당할 NetworkManager 싱글턴 객체 구현

---
+++ BoxOffice/Model/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  BoxOffice
+//
+//  Created by jyubong on 11/30/23.
+//
+
+import Foundation
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() { }
+}
