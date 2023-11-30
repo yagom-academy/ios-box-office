e886b4856b92e32887d7f2fcea0ed3e7c9cdf898
Hisop <khsdfg958@gmail.com>
2023-11-30 21:54:01 +0900

    feat: url Namespace추가

---
+++ BoxOffice/Model/Namespace.swift
@@ -0,0 +1,10 @@
+//
+//  Namespace.swift
+//  BoxOffice
+//
+//  Created by Hisop on 2023/11/30.
+//
+
+enum Request {
+    static let url: String = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=7d7554c98136c7fcead382ca5f9d5776&targetDt=20231130"
+}
