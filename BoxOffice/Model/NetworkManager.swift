60c94b579e90cd6b0dd50a27e0046e3fe410df2f
jyubong <nane030@naver.com>
2023-11-30 15:55:10 +0900

    feat: dailyboxoffice 데이터 불러올 fetchDailyBoxOffice 메서드 구현

--- BoxOffice/Model/NetworkManager.swift
+++ BoxOffice/Model/NetworkManager.swift
@@ -13,4 +13,25 @@ final class NetworkManager {
     let key = "3d65ed918572e0c8dc412bb3bf722f49"
     
     private init() { }
+    
+    func fetchDailyBoxOffice(at date: String, completion: @escaping (Movie?, Error?) -> Void) {
+        guard let url = URL(string: "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(key)&targetDt=\(date)") else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                return completion(nil, error)
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                (200...299).contains(httpResponse.statusCode) else {
+                return completion(nil, error)
+            }
+            
+            if let data = data, let movie = try? JSONDecoder().decode(Movie.self, from: data) {
+                completion(movie, nil)
+            }
+        }.resume()
+    }
 }
