b2cfdd9e392030c99aa5ea600dc07b05ac6980b2
jyubong <nane030@naver.com>
2023-11-30 16:23:18 +0900

    refactor: 접근제어 수정

--- BoxOffice/Model/NetworkManager.swift
+++ BoxOffice/Model/NetworkManager.swift
@@ -10,7 +10,7 @@ import Foundation
 final class NetworkManager {
     static let shared = NetworkManager()
     
-    let key = "3d65ed918572e0c8dc412bb3bf722f49"
+    private let key = "3d65ed918572e0c8dc412bb3bf722f49"
     
     private init() { }
     
