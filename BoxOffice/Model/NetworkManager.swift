7b3428d7ac9844985ac7fb366cb956cbf6b9d258
jyubong <nane030@naver.com>
2023-12-01 11:08:14 +0900

    refactor: networkManager fetchData를 제네릭 메서드로 변경 통합

--- BoxOffice/Model/NetworkManager.swift
+++ BoxOffice/Model/NetworkManager.swift
@@ -14,8 +14,8 @@ final class NetworkManager {
     
     private init() { }
     
-    func fetchDailyBoxOffice(at date: String, completion: @escaping (Result<[DailyBoxOfficeList], Error>) -> Void) {
-        guard let url = URL(string: "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(key)&targetDt=\(date)") else {
+    func fetchData<T: Decodable>(url: String, completion: @escaping (Result<T, Error>) -> Void) {
+        guard let url = URL(string: url) else {
             completion(.failure(FetchError.invalidURL))
             return
         }
@@ -32,43 +32,12 @@ final class NetworkManager {
                 return
             }
             
-            guard let data = data, let movie = try? JSONDecoder().decode(Movie.self, from: data) else {
+            guard let data = data, let movie = try? JSONDecoder().decode(T.self, from: data) else {
                 completion(.failure(FetchError.invalidData))
                 return
             }
             
-            let dailyBoxOfficeList = movie.boxOfficeResult.dailyBoxOfficeList
-            
-            completion(.success(dailyBoxOfficeList))
-        }.resume()
-    }
-    
-    func fetchMovieDetail(code: String, completion: @escaping (Result<MovieInfo, Error>) -> Void) {
-        guard let url = URL(string: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=\(key)&movieCd=\(code)") else {
-            completion(.failure(FetchError.invalidURL))
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse,
-                (200...299).contains(httpResponse.statusCode) else {
-                completion(.failure(FetchError.invalidResponse))
-                return
-            }
-            
-            guard let data = data, let movie = try? JSONDecoder().decode(MovieDetail.self, from: data) else {
-                completion(.failure(FetchError.invalidData))
-                return
-            }
-            
-            let movieInfo = movie.movieInfoResult.movieInfo
-            
-            completion(.success(movieInfo))
+            completion(.success(movie))
         }.resume()
     }
 }
