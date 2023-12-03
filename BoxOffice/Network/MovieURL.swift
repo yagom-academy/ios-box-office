257858250bf4875ca454da26c2ea4252e244ebb4
Morgan <devjoon@naver.com>
2023-12-03 15:00:23 +0900

    refactor: Key파일 분리 깃 ignore처리, NameSpace그룹 제거

--- BoxOffice/Network/MovieURL.swift
+++ BoxOffice/Network/MovieURL.swift
@@ -8,5 +8,4 @@
 enum MovieURL {
     static let kobisURL = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?"
     static let detailURL = "https://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?"
-    static let key = "d9538442725f83fb63d49ae6e965066a"
 }
