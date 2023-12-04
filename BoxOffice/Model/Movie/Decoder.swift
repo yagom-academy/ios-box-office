4a6e0a43a095c0449837e96341e8ba349a21b1e6
jyubong <nane030@naver.com>
2023-12-04 20:26:01 +0900

    feat: decoder 구조체 데이터 decode하는 parse 메서드 구현

--- BoxOffice/Model/Movie/Decoder.swift
+++ BoxOffice/Model/Movie/Decoder.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+struct Decoder {
+    func parse<T: Decodable>(data: Data, type: T.Type) throws -> T {
+        return try JSONDecoder().decode(type, from: data)
+    }
+}
