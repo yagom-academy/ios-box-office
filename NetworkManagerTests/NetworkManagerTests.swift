0c5742d440d6bf70c41cfc3d5f9d025306a35b47
jyubong <nane030@naver.com>
2023-12-04 20:48:36 +0900

    feat: networkManager data nil일때 test 추가

--- NetworkManagerTests/NetworkManagerTests.swift
+++ NetworkManagerTests/NetworkManagerTests.swift
@@ -10,9 +10,9 @@ import XCTest
 
 final class NetworkManagerTests: XCTestCase {
     var sut: NetworkManager!
-    private let api = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20220105"
     var url: URL!
     var jsonData: Data!
+    private let api = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20220105"
     
     override func setUpWithError() throws {
         url = try XCTUnwrap(URL(string: api))
@@ -64,6 +64,39 @@ final class NetworkManagerTests: XCTestCase {
         sut = .init(urlSession: URLSession(configuration: configuration))
         
         let expectation = FetchError.invalidResponse
+        
+        // when
+        sut.fetchData(url: api) { response in
+            if case let .failure(error) = response {
+                
+                guard let result = error as? FetchError else {
+                    XCTFail()
+                    return
+                }
+                
+                XCTAssertEqual(result, expectation)
+            }
+        }
+    }
+    
+    func test_fetchData_data_failure() {
+        // given
+        TestURLProtocol.loadingHandler = { [self] request in
+            let response = HTTPURLResponse(
+                url: url,
+                statusCode: 200,
+                httpVersion: nil,
+                headerFields: nil
+            )
+            return (nil, response, nil)
+        }
+        
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [TestURLProtocol.self]
+        
+        sut = .init(urlSession: URLSession(configuration: configuration))
+        
+        let expectation = FetchError.invalidData
         
         // when
         sut.fetchData(url: api) { response in
