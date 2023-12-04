e15cb53ed3ea6fc15a9f07dc051cf365ad3446ee
kiseok <carti1108@naver.com>
2023-12-04 20:44:48 +0900

    test: 테스트 수정

--- NetworkManagerTests/NetworkManagerTests.swift
+++ NetworkManagerTests/NetworkManagerTests.swift
@@ -11,22 +11,24 @@ import XCTest
 final class NetworkManagerTests: XCTestCase {
     var sut: NetworkManager!
     private let api = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20220105"
+    var url: URL!
+    var jsonData: Data!
+    
+    override func setUpWithError() throws {
+        url = try XCTUnwrap(URL(string: api))
+        jsonData = try XCTUnwrap(TestMovieJsonData.json.data(using: .utf8))
+    }
     
     func test_fetchData_success() {
         // given
-        guard let url = URL(string: api) else {
-            XCTFail()
-            return
-        }
-        
-        guard let data = TestMovieJsonData.json.data(using: .utf8) else {
-            XCTFail()
-            return
-        }
-        
-        TestURLProtocol.loadingHandler = { request in
-            let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
-            return (data, response, nil)
+        TestURLProtocol.loadingHandler = { [self] request in
+            let response = HTTPURLResponse(
+                url: url,
+                statusCode: 200,
+                httpVersion: nil,
+                headerFields: nil
+            )
+            return (jsonData, response, nil)
         }
         
         let configuration = URLSessionConfiguration.ephemeral
@@ -34,34 +36,26 @@ final class NetworkManagerTests: XCTestCase {
         
         sut = .init(urlSession: URLSession(configuration: configuration))
         
-        let expectation: Movie? = try? JSONDecoder().decode(Movie.self, from: data)
-        var result: Movie?
-        
         // when
-        sut.fetchData(url: api, dataType: Movie.self) { response in
-            if case let .success(movie) = response {
-                result = movie
+        sut.fetchData(url: api) { response in
+            if case let .success(data) = response {
+                let result = data
                 
-                XCTAssertEqual(result, expectation)
+                XCTAssertEqual(self.jsonData, result)
             }
         }
     }
     
     func test_fetchData_response_failure() {
         // given
-        guard let url = URL(string: api) else {
-            XCTFail()
-            return
-        }
-        
-        guard let data = TestMovieJsonData.json.data(using: .utf8) else {
-            XCTFail()
-            return
-        }
-        
-        TestURLProtocol.loadingHandler = { request in
-            let response = HTTPURLResponse(url: url, statusCode: 400, httpVersion: nil, headerFields: nil)
-            return (data, response, nil)
+        TestURLProtocol.loadingHandler = { [self] request in
+            let response = HTTPURLResponse(
+                url: url,
+                statusCode: 400,
+                httpVersion: nil,
+                headerFields: nil
+            )
+            return (jsonData, response, nil)
         }
         
         let configuration = URLSessionConfiguration.ephemeral
@@ -72,7 +66,7 @@ final class NetworkManagerTests: XCTestCase {
         let expectation = FetchError.invalidResponse
         
         // when
-        sut.fetchData(url: api, dataType: Movie.self) { response in
+        sut.fetchData(url: api) { response in
             if case let .failure(error) = response {
                 
                 guard let result = error as? FetchError else {
