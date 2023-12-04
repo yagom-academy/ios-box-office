0111bd638a073f89d85a1143ddc72708c6651699
kiseok <carti1108@naver.com>
2023-12-04 20:08:42 +0900

    feat: 구조체에서 Enum 타입으로 변경

--- BoxOffice/Model/Network/URLManager.swift
+++ BoxOffice/Model/Network/URLManager.swift
@@ -5,15 +5,19 @@
 //  Created by jyubong on 12/1/23.
 //
 
-struct URLManager {
-    private let baseURL = "http://kobis.or.kr/kobisopenapi/webservice/rest/"
-    private let key = "3d65ed918572e0c8dc412bb3bf722f49"
+enum URLManager {
+    case dailyBoxOffice(date: String)
+    case movieDetail(code: String)
     
-    func dailyBoxOfficeURL(date: String) -> String {
-        return "\(baseURL)boxoffice/searchDailyBoxOfficeList.json?key=\(key)&targetDt=\(date)"
-    }
-    
-    func movieDetailURL(code: String) -> String {
-        return "\(baseURL)movie/searchMovieInfo.json?key=\(key)&movieCd=\(code)"
+    var url: String {
+        let baseURL = "http://kobis.or.kr/kobisopenapi/webservice/rest/"
+        let key = "3d65ed918572e0c8dc412bb3bf722f49"
+        
+        switch self {
+        case .dailyBoxOffice(let date):
+            return "\(baseURL)boxoffice/searchDailyBoxOfficeList.json?key=\(key)&targetDt=\(date)"
+        case .movieDetail(let code):
+            return "\(baseURL)movie/searchMovieInfo.json?key=\(key)&movieCd=\(code)"
+        }
     }
 }
