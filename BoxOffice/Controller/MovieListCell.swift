866569fca57f18b854821acdbe57ad620cd7ab5e
jyubong <nane030@naver.com>
2023-12-05 15:16:47 +0900

    feat: MovieListCell 내 label, stackview 구현

--- BoxOffice/Controller/MovieListCell.swift
+++ BoxOffice/Controller/MovieListCell.swift
@@ -9,5 +9,53 @@ import UIKit
 
 @available(iOS 14.0, *)
 class MovieListCell: UICollectionViewListCell {
+    private var rankLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 22)
+        
+        return label
+    }()
     
+    private var rankFluctuationLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+            
+        return label
+    }()
+    
+    private lazy var rankStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 5
+        
+        stackView.addSubview(rankLabel)
+        stackView.addSubview(rankFluctuationLabel)
+        return stackView
+    }()
+    
+    private var movieNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20)
+            
+        return label
+    }()
+    
+    private var audienceCountLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18)
+            
+        return label
+    }()
+    
+    private lazy var movieStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 3
+        
+        stackView.addSubview(movieNameLabel)
+        stackView.addSubview(audienceCountLabel)
+        return stackView
+    }()
 }
