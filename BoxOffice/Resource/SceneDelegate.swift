a5f8feafc824f969681b714c3026b26bf9f2217f
jyubong <nane030@naver.com>
2023-12-05 15:16:08 +0900

    style: 주석 제거

--- BoxOffice/Resource/SceneDelegate.swift
+++ BoxOffice/Resource/SceneDelegate.swift
@@ -12,8 +12,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
-//    @available(iOS 15.0, *)
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else {
             return
