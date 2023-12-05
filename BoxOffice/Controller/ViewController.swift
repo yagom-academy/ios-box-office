a08cdb1215c7f90571afa7069daa418ee7de69ea
jyubong <nane030@naver.com>
2023-12-05 14:32:12 +0900

    feat: collectionview 코드로 생성

--- BoxOffice/Controller/ViewController.swift
+++ BoxOffice/Controller/ViewController.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .systemYellow
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
