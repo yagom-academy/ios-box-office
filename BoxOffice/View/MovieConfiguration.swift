ac0cc9d095f40a3081b05e9d28aef699faed92dd
kiseok <carti1108@naver.com>
2023-12-07 15:01:13 +0900

    docs: MovieConfiguration, MovieContentView 파일 생성
    MovieListCell 파일 위치 변경

---
+++ BoxOffice/View/MovieConfiguration.swift
@@ -0,0 +1,8 @@
+//
+//  MovieConfiguration.swift
+//  BoxOffice
+//
+//  Created by Kiseok on 12/7/23.
+//
+
+import Foundation
---
+++ BoxOffice/View/MovieContentView.swift
@@ -0,0 +1,8 @@
+//
+//  MovieContentView.swift
+//  BoxOffice
+//
+//  Created by Kiseok on 12/7/23.
+//
+
+import Foundation
