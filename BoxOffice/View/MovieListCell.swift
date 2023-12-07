b213956db915aabf04d8708e91248cfe3a92d46f
kiseok <carti1108@naver.com>
2023-12-07 15:57:49 +0900

    feat: updateConfiguration 메서드 구현

--- BoxOffice/View/MovieListCell.swift
+++ BoxOffice/View/MovieListCell.swift
@@ -8,5 +8,18 @@
 import UIKit
 
 class MovieListCell: UICollectionViewListCell {
-
+    var movie: DailyBoxOfficeList?
+    
+    override func updateConfiguration(using state: UICellConfigurationState) {
+        var newConfiguration = MovieConfiguration().updated(for: state)
+        newConfiguration.rank = movie?.rank
+        newConfiguration.rankFluctuation = movie?.rankFluctuation
+        newConfiguration.movieName = movie?.movieName
+        newConfiguration.audienceCount = movie?.audienceCount
+        newConfiguration.audienceAccumulation = movie?.audienceAccumulation
+        
+        contentConfiguration = newConfiguration
+        
+        self.accessories = [.disclosureIndicator()]
+    }
 }
