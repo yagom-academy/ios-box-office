77e1e5a3bef6350b23d4d20db2506836c4979a00
kiseok <carti1108@naver.com>
2023-12-07 15:13:47 +0900

    feat: configuration 변수 생성

--- BoxOffice/View/MovieContentView.swift
+++ BoxOffice/View/MovieContentView.swift
@@ -8,5 +8,15 @@
 import UIKit
 
 class MovieContentView: UIView, UIContentView {
+    var configuration: UIContentConfiguration
     
+    init(configuration: UIContentConfiguration) {
+        self.configuration = configuration
+        
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
