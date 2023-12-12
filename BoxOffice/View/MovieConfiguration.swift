967722d29d0b9551ac43ff2d7baf6928f800aed7
kiseok <carti1108@naver.com>
2023-12-12 20:13:43 +0900

    feat: rankOldAndNew 변수 추가

--- BoxOffice/View/MovieConfiguration.swift
+++ BoxOffice/View/MovieConfiguration.swift
@@ -13,6 +13,7 @@ struct MovieConfiguration: UIContentConfiguration, Hashable {
     var movieName: String?
     var audienceCount: String?
     var audienceAccumulation: String?
+    var rankOldAndNew: RankOldAndNew?
     
     func makeContentView() -> UIView & UIContentView {
         return MovieContentView(configuration: self)
